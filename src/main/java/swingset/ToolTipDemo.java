146551a0cf51f0348bbee7d05ef8c29f17ee2800
homebeaver <homebeaver@outlook.de>
2022-01-19 23:46:41 +0100

    reorg class ToolTipDemo without controller

--- src/main/java/swingset/ToolTipDemo.java
+++ src/main/java/swingset/ToolTipDemo.java
@@ -4,64 +4,100 @@ Copyright notice, list of conditions and disclaimer see LICENSE file
 package swingset;
 
 import java.awt.Color;
-import java.awt.Dimension;
+import java.awt.Frame;
 import java.awt.Point;
 import java.awt.Polygon;
 
-import javax.swing.Box;
 import javax.swing.BoxLayout;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
+import javax.swing.border.BevelBorder;
+
+import org.jdesktop.swingx.JXLabel;
+import org.jdesktop.swingx.JXPanel;
 
 /**
  * ToolTip Demo
  *
  * @author Jeff Dinkins
+ * @author EUG https://github.com/homebeaver (reorg)
  */
-public class ToolTipDemo extends DemoModule {
+public class ToolTipDemo extends AbstractDemo {
 
     public static final String ICON_PATH = "toolbar/ToolTip.gif";
+
+    JXPanel presentationPanel = null;
+
+    @Override
+    public JXPanel getDemoPane() {
+    	if(presentationPanel!=null) {
+    		return presentationPanel;
+    	}
+    	presentationPanel = new JXPanel();
+//    	presentationPanel.setPreferredSize(PREFERRED_SIZE);
+    	presentationPanel.setBorder(new BevelBorder(BevelBorder.LOWERED));
+
+    	presentationPanel.setLayout(new BoxLayout(presentationPanel, BoxLayout.Y_AXIS));
+    	presentationPanel.setBackground(Color.white);
+
+        // Create a Cow to put in the center of the panel.
+        Cow cow = new Cow();
+        cow.getAccessibleContext().setAccessibleName(getString("accessible_cow"));
+
+        // Set the tooltip text. Note, for fun, we also set more tooltip text
+        // descriptions for the cow down below in the Cow.contains() method.
+        cow.setToolTipText(getString("cow"));
+
+        // Add the cow midway down the panel
+//        presentationPanel.add(Box.createRigidArea(new Dimension(1, 150)));
+        presentationPanel.add(cow);
+        return presentationPanel;
+    }
+
+    @Override
+	public JXPanel getControlPane() {
+		// no controller
+		return new JXPanel(); // TODO default EmptyControler
+	}
 
     /**
      * main method allows us to run as a standalone demo.
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) { // TODO
         ToolTipDemo demo = new ToolTipDemo(null);
-        demo.mainImpl();
+//        demo.mainImpl();
     }
 
     /**
      * ToolTipDemo Constructor
      */
-    public ToolTipDemo(SwingSet2 swingset) {
+    public ToolTipDemo(Frame frame) {
         // Set the title for this demo, and an icon used to represent this
         // demo inside the SwingSet2 app.
-        super(swingset, "ToolTipDemo", ICON_PATH);
+//        super(swingset, "ToolTipDemo", ICON_PATH);
 
-        // Set the layout manager.
-        JPanel p = getDemoPanel();
-        p.setLayout(new BoxLayout(p, BoxLayout.Y_AXIS));
-        p.setBackground(Color.white);
-
-        // Create a Cow to put in the center of the panel.
-        Cow cow = new Cow();
-        cow.getAccessibleContext().setAccessibleName(getString("ToolTipDemo.accessible_cow"));
-
-        // Set the tooltip text. Note, for fun, we also set more tooltip text
-        // descriptions for the cow down below in the Cow.contains() method.
-        cow.setToolTipText(getString("ToolTipDemo.cow"));
-
-        // Add the cow midway down the panel
-        p.add(Box.createRigidArea(new Dimension(1, 150)));
-        p.add(cow);
+//        // Set the layout manager.
+//        JPanel p = getDemoPanel();
+//        p.setLayout(new BoxLayout(p, BoxLayout.Y_AXIS));
+//        p.setBackground(Color.white);
+//
+//        // Create a Cow to put in the center of the panel.
+//        Cow cow = new Cow();
+//        cow.getAccessibleContext().setAccessibleName(getString("ToolTipDemo.accessible_cow"));
+//
+//        // Set the tooltip text. Note, for fun, we also set more tooltip text
+//        // descriptions for the cow down below in the Cow.contains() method.
+//        cow.setToolTipText(getString("ToolTipDemo.cow"));
+//
+//        // Add the cow midway down the panel
+//        p.add(Box.createRigidArea(new Dimension(1, 150)));
+//        p.add(cow);
     }
 
 
-    class Cow extends JLabel {
+    class Cow extends JXLabel {
         Polygon cowgon = new Polygon();
 
         public Cow() {
-            super(createImageIcon("tooltip/cow.gif", getString("ToolTipDemo.bessie")));
+            super(StaticUtilities.createImageIcon("tooltip/cow.gif"));
             setAlignmentX(CENTER_ALIGNMENT);
 
             // Set polygon points that define the outline of the cow.
@@ -94,7 +130,7 @@ public class ToolTipDemo extends DemoModule {
             if((x > 30) && (x < 60) && (y > 60) && (y < 85)) {
                 if(!moo) {
                     setToolTipText("<html><center><font color=blue size=+2>" +
-                                   getString("ToolTipDemo.moo") + "</font></center></html>");
+                                   getString("moo") + "</font></center></html>");
                     moo = true;
                     milk = false;
                     tail = false;
@@ -102,20 +138,20 @@ public class ToolTipDemo extends DemoModule {
             } else if((x > 150) && (x < 260) && (y > 90) && (y < 145)) {
                 if(!milk) {
                     setToolTipText("<html><center><font face=AvantGarde size=+1 color=white>" +
-                                   getString("ToolTipDemo.got_milk") + "</font></center></html>");
+                                   getString("got_milk") + "</font></center></html>");
                     milk = true;
                     moo = false;
                     tail = false;
                 }
             } else if((x > 280) && (x < 300) && (y > 20) && (y < 175)) {
                 if(!tail) {
-                    setToolTipText("<html><em><b>" + getString("ToolTipDemo.tail") + "</b></em></html>");
+                    setToolTipText("<html><em><b>" + getString("tail") + "</b></em></html>");
                     tail = true;
                     moo = false;
                     milk = false;
                 }
             } else if(moo || milk || tail) {
-                setToolTipText(getString("ToolTipDemo.tooltip_features"));
+                setToolTipText(getString("tooltip_features"));
                 moo = false;
                 tail = false;
                 milk = false;
