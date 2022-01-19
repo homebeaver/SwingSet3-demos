eb9332677a5cc447e2a32a5c483907811eb99668
homebeaver <homebeaver@outlook.de>
2022-01-19 23:12:03 +0100

    doku expand + collapseAll : method in XTree
    TODO implement controller

--- src/main/java/swingset/TreeDemo.java
+++ src/main/java/swingset/TreeDemo.java
@@ -4,6 +4,7 @@ Copyright notice, list of conditions and disclaimer see LICENSE file
 package swingset;
 
 import java.awt.BorderLayout;
+import java.awt.Frame;
 import java.awt.Insets;
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -12,43 +13,87 @@ import java.io.InputStreamReader;
 import java.net.URL;
 
 import javax.swing.JScrollPane;
-import javax.swing.JTree;
+import javax.swing.border.BevelBorder;
 import javax.swing.tree.DefaultMutableTreeNode;
+
+import org.jdesktop.swingx.JXButton;
+import org.jdesktop.swingx.JXPanel;
+import org.jdesktop.swingx.JXTree;
 
 /**
  * JTree Demo
  *
- * @author Jeff Dinkins
+ * @author Jeff Dinkins (inception)
+ * @author EUG https://github.com/homebeaver (reorg)
  */
-public class TreeDemo extends DemoModule {
+public class TreeDemo extends AbstractDemo {
 
-	private static final long serialVersionUID = -932066695707626601L;
+//	private static final long serialVersionUID = -932066695707626601L;
 
 	public static final String ICON_PATH = "toolbar/JTree.gif";
 
-    JTree tree;
+    JXPanel presentationPanel = null;
+    JXTree tree;
+
+    @Override
+    public JXPanel getDemoPane() {
+    	if(presentationPanel!=null) {
+    		return presentationPanel;
+    	}
+    	presentationPanel = new JXPanel(new BorderLayout());
+    	presentationPanel.setPreferredSize(PREFERRED_SIZE);
+    	presentationPanel.setBorder(new BevelBorder(BevelBorder.LOWERED));
+
+        presentationPanel.add(createTree(), BorderLayout.CENTER);
+        return presentationPanel;
+    }
+    
+    private JXButton expandButton;
+    private JXButton collapseButton;
+/*
+        expandButton.setAction(DemoUtils.getAction(this, "expandAll"));
+        collapseButton.setAction(DemoUtils.getAction(this, "collapseAll"));
+    // <snip> JXTree convenience api
+    // expand/collapse all nodes
+    @Action
+    public void expandAll() {
+        tree.expandAll();
+    }
+
+    @Action
+    public void collapseAll() {
+        tree.collapseAll();
+    }
+    // </snip>
+
+ */
+    public void collapseAll() {
+        tree.collapseAll(); // method in XTree
+    }
+
+    
+    @Override
+	public JXPanel getControlPane() {
+		// no controller TODO alles ausklappen! ==> expandButton
+		return new JXPanel(); // TODO default EmptyControler
+	}
 
     /**
      * main method allows us to run as a standalone demo.
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) { // TODO
         TreeDemo demo = new TreeDemo(null);
-        demo.mainImpl();
+//        demo.mainImpl();
     }
 
     /**
      * TreeDemo Constructor
      */
-    public TreeDemo(SwingSet2 swingset) {
-        // Set the title for this demo, and an icon used to represent this
-        // demo inside the SwingSet2 app.
-        super(swingset, "TreeDemo", ICON_PATH);
-
-        getDemoPanel().add(createTree(), BorderLayout.CENTER);
+    public TreeDemo(Frame frame) {
     }
 
     public JScrollPane createTree() {
-        DefaultMutableTreeNode top = new DefaultMutableTreeNode(getString("TreeDemo.music"));
+        DefaultMutableTreeNode top = new DefaultMutableTreeNode(getString("music"));
         DefaultMutableTreeNode catagory = null ;
         DefaultMutableTreeNode artist = null;
         DefaultMutableTreeNode record = null;
@@ -95,7 +140,7 @@ public class TreeDemo extends DemoModule {
         } catch (IOException e) {
         }
 
-        tree = new JTree(top) {
+        tree = new JXTree(top) {
             public Insets getInsets() {
                 return new Insets(5,5,5,5);
             }
