484e1678fa16536097292fa0df08ba17656d13f4
homebeaver <homebeaver@outlook.de>
2022-01-31 17:37:18 +0100

    use currentDemoFrame and remove Map demos

--- src/main/java/com/klst/swingx/RootFrame.java
+++ src/main/java/com/klst/swingx/RootFrame.java
@@ -2,11 +2,8 @@ package com.klst.swingx;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
-import java.awt.Component;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.logging.Logger;
 
 import javax.swing.AbstractAction;
@@ -15,7 +12,6 @@ import javax.swing.Action;
 import javax.swing.BorderFactory;
 import javax.swing.Icon;
 import javax.swing.JMenu;
-import javax.swing.JPopupMenu;
 import javax.swing.JScrollPane;
 import javax.swing.JTabbedPane;
 import javax.swing.UIManager;
@@ -27,8 +23,6 @@ import org.jdesktop.swingx.JXFrame;
 import org.jdesktop.swingx.JXLabel;
 import org.jdesktop.swingx.JXPanel;
 import org.jdesktop.swingx.JXTree;
-import org.jdesktop.swingx.demos.tree.XTreeDemo;
-import org.jdesktop.swingx.demos.treetable.TreeTableDemo;
 import org.jdesktop.swingx.icon.PlayIcon;
 import org.jdesktop.swingx.icon.SizingConstants;
 import org.jdesktop.swingx.icon.StopIcon;
@@ -37,23 +31,6 @@ import org.jdesktop.swingx.renderer.IconValue;
 import org.jdesktop.swingx.renderer.StringValue;
 import org.jdesktop.swingx.renderer.StringValues;
 import org.jdesktop.swingx.treetable.TreeTableModel;
-
-import swingset.ButtonDemo;
-import swingset.ColorChooserDemo;
-import swingset.ComboBoxDemo;
-import swingset.FileChooserDemo;
-import swingset.HtmlDemo;
-import swingset.ListDemo;
-import swingset.OptionPaneDemo;
-import swingset.ProgressBarDemo;
-import swingset.ScrollPaneDemo;
-import swingset.SliderDemo;
-import swingset.SplitPaneDemo;
-import swingset.StaticUtilities;
-import swingset.TabbedPaneDemo;
-import swingset.TableDemo;
-import swingset.ToolTipDemo;
-import swingset.TreeDemo;
 
 /*
 TODO RootFrame <> JXRootPane
@@ -65,32 +42,13 @@ public class RootFrame extends WindowFrame {
 
 	private static final String TITLE = "Gossip";
 	
-	Map<Class<?>, WindowFrame> demos;
+//	Map<Class<?>, WindowFrame> demos;
+	WindowFrame currentDemoFrame = null;
+	
 	public RootFrame() {
 		super(TITLE);
 		super.rootFrame = this;
         UIManager.put("swing.boldMetal", Boolean.FALSE); // turn off bold fonts in Metal
-		demos = new HashMap<Class<?>, WindowFrame>();
-		// TODO ... so initialisieren geht angeblich auch für static var
-//		demos = Map.ofEntries(
-//			Map.entry(InternalFrameDemo.class, null), // NullPointerException at java.base/java.util.Objects.requireNonNull(Objects.java:208)
-//			Map.entry(ButtonDemo.class       , null),
-//			Map.entry(ColorChooserDemo.class , null),
-//			Map.entry(ComboBoxDemo.class     , null),
-//			Map.entry(FileChooserDemo.class  , null),
-//			Map.entry(HtmlDemo.class         , null),
-//			Map.entry(ListDemo.class         , null),
-//			Map.entry(OptionPaneDemo.class   , null),
-//			Map.entry(ProgressBarDemo.class  , null),
-//			Map.entry(ScrollPaneDemo.class   , null),
-//			Map.entry(SliderDemo.class       , null),
-//			Map.entry(SplitPaneDemo.class    , null),
-//			Map.entry(TabbedPaneDemo.class   , null),
-//			Map.entry(TableDemo.class        , null),
-//			Map.entry(ToolTipDemo.class      , null),
-//			Map.entry(TreeDemo.class         , null)
-//			);
-		addDemos();
 		frames = new ArrayList<JXFrame>();
 		frames.add(this);
 		//setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -162,67 +120,9 @@ CENTER: JXPanel currentController
 	// SwingSet3 Data: JXTable, JXList, JXTree, JXTreeTable
 	static Icon SS3DATA_ICON = new PlayIcon(SizingConstants.SMALL_ICON, Color.BLUE);
 	List<AbstractAction> demoActions = new ArrayList<AbstractAction>();
-	private void addDemos() {
-		demos.put(null, null); // current presentation frame
-//		demoActions.add(new DemoAction(ButtonDemo.class, "runDemo", StaticUtilities.createImageIcon(ButtonDemo.ICON_PATH)));
-//		demos.put(ButtonDemo.class, null);
-//		demoActions.add(new DemoAction(ColorChooserDemo.class, "runDemo", StaticUtilities.createImageIcon(ColorChooserDemo.ICON_PATH)));
-//		demos.put(ColorChooserDemo.class, null);
-//		demoActions.add(new DemoAction(ComboBoxDemo.class, "runDemo", StaticUtilities.createImageIcon(ComboBoxDemo.ICON_PATH)));
-//		demos.put(ComboBoxDemo.class, null);
-//		demoActions.add(new DemoAction(FileChooserDemo.class, "runDemo", StaticUtilities.createImageIcon(FileChooserDemo.ICON_PATH)));
-//		demos.put(FileChooserDemo.class, null);
-//		demoActions.add(new DemoAction(HtmlDemo.class, "runDemo", StaticUtilities.createImageIcon(HtmlDemo.ICON_PATH)));
-//		demos.put(HtmlDemo.class, null);
-//		demoActions.add(new DemoAction(ListDemo.class, "runDemo", StaticUtilities.createImageIcon(ListDemo.ICON_PATH)));
-//		demos.put(ListDemo.class, null);
-//		demoActions.add(new DemoAction(OptionPaneDemo.class, "runDemo", StaticUtilities.createImageIcon(OptionPaneDemo.ICON_PATH)));
-//		demos.put(OptionPaneDemo.class, null);		
-//		demoActions.add(new DemoAction(ProgressBarDemo.class, "runDemo", StaticUtilities.createImageIcon(ProgressBarDemo.ICON_PATH)));
-//		demos.put(ProgressBarDemo.class, null);
-//		demoActions.add(new DemoAction(ScrollPaneDemo.class, "runDemo", StaticUtilities.createImageIcon(ScrollPaneDemo.ICON_PATH)));
-//		demos.put(ScrollPaneDemo.class, null);
-//		demoActions.add(new DemoAction(SliderDemo.class, "Slider", SS2_ICON, StaticUtilities.createImageIcon(SliderDemo.ICON_PATH)));
-//		demos.put(SliderDemo.class, null);
-//		demoActions.add(new DemoAction(SplitPaneDemo.class, "SplitPane", SS2_ICON, StaticUtilities.createImageIcon(SplitPaneDemo.ICON_PATH)));
-//		demos.put(SplitPaneDemo.class, null);
-//		demoActions.add(new DemoAction(TabbedPaneDemo.class, "TabbedPane", SS2_ICON, StaticUtilities.createImageIcon(TabbedPaneDemo.ICON_PATH)));
-//		demos.put(TabbedPaneDemo.class, null);
-//		demoActions.add(new DemoAction(TableDemo.class, "Table", SS2_ICON, StaticUtilities.createImageIcon(TableDemo.ICON_PATH)));
-//		demos.put(TableDemo.class, null);
-//		demoActions.add(new DemoAction(ToolTipDemo.class, "ToolTip", SS2_ICON, StaticUtilities.createImageIcon(ToolTipDemo.ICON_PATH)));
-//		demos.put(ToolTipDemo.class, null);
-//		demoActions.add(new DemoAction(TreeDemo.class, "Tree", SS2_ICON, StaticUtilities.createImageIcon(TreeDemo.ICON_PATH)));
-//		demos.put(TreeDemo.class, null);
-//		// swingset 3:
-//		demoActions.add(new DemoAction(XTreeDemo.class, "XTree", SS3DATA_ICON, StaticUtilities.createImageIcon(XTreeDemo.class, XTreeDemo.ICON_PATH)));
-//		demos.put(XTreeDemo.class, null);
-//		demoActions.add(new DemoAction(TreeTableDemo.class, "XTreeTable", SS3DATA_ICON, StaticUtilities.createImageIcon(TreeTableDemo.class, TreeTableDemo.ICON_PATH)));
-//		demos.put(TreeTableDemo.class, null);
-	}
 
 	JMenu menu;
     public JMenu createDemosMenu() {
-//    	this.menu = new JMenu(new DemoAction(null, "Demos"));
-//    	AbstractAction ss2Action = new DemoAction(null, "SwingSet2", SS2_ICON, null); 	
-//    	JMenu ss2menu = (JMenu) menu.add(new JMenu(ss2Action));
-//    	AbstractAction ss3Action = new DemoAction(null, "SwingSet3", PLAY_ICON, null); 	
-//    	JMenu ss3menu = (JMenu) menu.add(new JMenu(ss3Action));
-//    	
-//    	this.demoActions.forEach( action -> {
-//    		if(SS2_ICON==action.getValue(Action.SMALL_ICON)) {
-//            	ss2menu.add(action);
-//    		} else if(action.getValue(Action.SMALL_ICON)!=null) {
-//    			ss3menu.add(action);
-//    		}
-//    	});
-//    	
-//		Component[] compArray = menu.getMenuComponents();
-//		for(int i=0; i<compArray.length; i++) {
-//			JMenu comp = (JMenu)compArray[i];
-//			LOG.info("comp "+i + ":"+comp.getAction());
-//		}
-    	
     	//DemoMenuAction root = DemoMenuAction.getRootAction();
 		TreeNode rootNode = DemoMenuAction.createTree(this);
     	TreeTableModel model = DemoMenuAction.createMenuModel(rootNode);
@@ -308,12 +208,12 @@ CENTER: JXPanel currentController
     		WindowFrame frame = new WindowFrame("Frame number " + frameNumber, rootFrame, window_ID, object);
     		frames.add(frame);
     		// close/dispose current and make frame current:
-    		WindowFrame current = demos.get(null);
+    		WindowFrame current = currentDemoFrame;
     		LOG.info("------------ close/dispose "+current);
     		if(current!=null) {
     			current.dispose();
     		}
-    		demos.put(null, frame);
+    		currentDemoFrame = frame;
     		
     		return frame;
 		}
