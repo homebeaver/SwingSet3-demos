35ce8d479900f68c78815ac89a3bfa41557ad291
homebeaver <homebeaver@outlook.de>
2022-02-20 22:11:11 +0100

    trident : konflikt mit org.xml.sax.*
    jar durch Teile der src org.pushingpixels.trident ersetzt

---
+++ src/main/java/org/pushingpixels/trident/package-info.java
@@ -0,0 +1,6 @@
+/*
+wg. Konflikt mit org.xml.sax.* : The package org.xml.sax.helpers is accessible from more than one module
+kopiere ich hier Teile!!! der src
+
+ */
+package org.pushingpixels.trident;
