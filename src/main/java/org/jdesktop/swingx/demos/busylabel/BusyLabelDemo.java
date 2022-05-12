aa130e19387ce664846f321c5e83a95558dcd5b0
homebeaver <homebeaver@outlook.de>
2022-05-12 17:01:16 +0200

    default example + more complicated example in SOUTH
    TODO size dynamisch änderbar

--- src/main/java/org/jdesktop/swingx/demos/busylabel/BusyLabelDemo.java
+++ src/main/java/org/jdesktop/swingx/demos/busylabel/BusyLabelDemo.java
@@ -5,25 +5,40 @@ package org.jdesktop.swingx.demos.busylabel;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.Component;
 import java.awt.Dimension;
 import java.awt.Frame;
 import java.awt.GradientPaint;
 import java.awt.geom.Point2D;
+import java.awt.geom.Rectangle2D;
+import java.awt.geom.Rectangle2D;
+import java.awt.geom.RoundRectangle2D;
+
 import java.util.Dictionary;
 import java.util.Hashtable;
 import java.util.logging.Logger;
 
+import javax.swing.Box;
+import javax.swing.BoxLayout;
 import javax.swing.JComponent;
 import javax.swing.JLabel;
+import javax.swing.JPanel;
 import javax.swing.JSlider;
 import javax.swing.SwingUtilities;
 import javax.swing.border.BevelBorder;
+import javax.swing.border.Border;
+import javax.swing.border.CompoundBorder;
+import javax.swing.border.EmptyBorder;
+import javax.swing.border.SoftBevelBorder;
+import javax.swing.border.TitledBorder;
 
 import org.jdesktop.swingx.JXBusyLabel;
 import org.jdesktop.swingx.JXFrame;
 import org.jdesktop.swingx.JXFrame.StartPosition;
 import org.jdesktop.swingx.JXPanel;
 import org.jdesktop.swingx.VerticalLayout;
+import org.jdesktop.swingx.icon.EmptyIcon;
+import org.jdesktop.swingx.painter.BusyPainter;
 import org.jdesktop.swingx.painter.MattePainter;
 import org.jdesktop.swingx.util.PaintUtils;
 
@@ -55,9 +70,13 @@ public class BusyLabelDemo extends AbstractDemo {
 	private static final Logger LOG = Logger.getLogger(BusyLabelDemo.class.getName());
 	private static final String DESCRIPTION = "Demonstrates JXBusyLabel, a control for identifying busy periods";
 
+	// controller prop name
+	private static final String SIZE_SLIDER = "sizeSlider";
+	
 	private JXBusyLabel label;
     
     // Controller:
+    private JSlider sizeSlider;   
     private JSlider speedSlider;   
     private JSlider pointsSlider;
     private JSlider trailSlider;
@@ -112,12 +131,70 @@ public class BusyLabelDemo extends AbstractDemo {
 //    	label.getBusyPainter().setBaseColor(label.getBusyPainter().getBaseColor().brighter());
     	label.setBusy(true);
     	add(label, BorderLayout.EAST); // EAST, damit controls den label nicht überdeckt
+    	
+    	// default example:
+    	JXBusyLabel busyLabel = new JXBusyLabel();
+    	LOG.info("default PreferredSize:"+busyLabel.getPreferredSize()); // java.awt.Dimension[width=26,height=26]
+//    	add(busyLabel, BorderLayout.WEST);
+    	busyLabel.setBusy(true);
+    	
+    	// more complicated example:
+    	JXBusyLabel mcLabel = new JXBusyLabel(new Dimension(100,84));
+    	BusyPainter painter = new BusyPainter(
+    			new Rectangle2D.Float(0, 0, 13.500001f, 1),  // point shape
+    			new RoundRectangle2D.Float(12.5f, 12.5f, 59.0f, 59.0f, 10, 10)  // trajectory 
+    			);
+    	painter.setTrailLength(5);
+    	painter.setPoints(31);
+    	painter.setFrame(1);
+    	mcLabel.setPreferredSize(new Dimension(100,84));
+//    	mcLabel.setIcon(new EmptyIcon(100,84));
+    	mcLabel.setBusyPainter(painter);
+//     	add(mcLabel, BorderLayout.CENTER);
+     	mcLabel.setBusy(true);
+    	
+    	JPanel p = createHorizontalPanel(false); // true == loweredBorder from super
+//    	JPanel p = createVerticalPanel(false);
+    	p.add(Box.createRigidArea(HGAP30));
+    	p.add(busyLabel);
+    	p.add(Box.createRigidArea(HGAP30));
+    	p.setBorder(new TitledBorder(null, "default and more complicated example", TitledBorder.LEFT, TitledBorder.TOP));
+    	p.add(mcLabel);
+    	add(p, BorderLayout.SOUTH);   	
     }
 
 	@Override
 	public JXPanel getControlPane() {
     	JXPanel controls = new JXPanel(new VerticalLayout());
     	controls.setName("controlPanel");
+
+        sizeSlider = new JSlider();
+        sizeSlider.setName(SIZE_SLIDER);
+        sizeSlider.setOpaque(Boolean.parseBoolean(getBundleString(SIZE_SLIDER+".opaque", Boolean.toString(false))));
+        sizeSlider.setPaintLabels(Boolean.valueOf(getBundleString(SIZE_SLIDER+".paintLabels", Boolean.toString(true))));
+        sizeSlider.setMinimum(Integer.parseInt(getBundleString(SIZE_SLIDER+".minimum", "10")));
+        sizeSlider.setMaximum(Integer.parseInt(getBundleString(SIZE_SLIDER+".maximum", "100")));
+        sizeSlider.setValue(Integer.parseInt(getBundleString(SIZE_SLIDER+".value", "26")));
+        Dictionary<Integer, JComponent> labels = new Hashtable<Integer, JComponent>();
+        String labelTable = getBundleString(SIZE_SLIDER+".labelTable");
+        LOG.info("sizeSlider.labelTable:"+labelTable);
+        labels.put(sizeSlider.getMinimum(), new JLabel("Smaller"));
+        labels.put(sizeSlider.getMaximum(), new JLabel("Bigger"));
+        sizeSlider.setLabelTable(labels);
+        sizeSlider.addChangeListener( ce -> {
+        	LOG.info("BusyPainter.Size aka Dimension:"+sizeSlider.getValue());
+//        	label.setBusy(false);
+//        	label.setPreferredSize(new Dimension(sizeSlider.getValue(),sizeSlider.getValue()));
+//        	label.setBusy(true);
+// Das funktioniert so nicht! ==> ChangeListener
+        	remove(label);
+        	this.repaint(getVisibleRect());
+        	label = new JXBusyLabel(new Dimension(sizeSlider.getValue(),sizeSlider.getValue()));
+        	add(label, BorderLayout.WEST);
+        	label.setBusy(true);
+        	this.repaint(getVisibleRect());
+    	});
+//        controls.add(sizeSlider); // TODO zuerst muss size dynamisch änderbar sein
 
         speedSlider = new JSlider();
         speedSlider.setName("speedSlider");
@@ -128,13 +205,18 @@ speedSlider.minimum=1
 speedSlider.maximum=50
 speedSlider.value=41
  */
+        // use Boolean.valueOf or Boolean.parseBoolean:
         speedSlider.setOpaque(Boolean.parseBoolean(getBundleString("speedSlider.opaque")));
-        speedSlider.setPaintLabels(Boolean.parseBoolean(getBundleString("speedSlider.paintLabels")));
+        speedSlider.setPaintLabels(Boolean.valueOf(getBundleString("speedSlider.paintLabels")));
         speedSlider.setMinimum(Integer.parseInt(getBundleString("speedSlider.minimum")));
         speedSlider.setMaximum(Integer.parseInt(getBundleString("speedSlider.maximum")));
         speedSlider.setValue(Integer.parseInt(getBundleString("speedSlider.value")));
-        Dictionary<Integer, JComponent> labels = new Hashtable<Integer, JComponent>();
+        labels = new Hashtable<Integer, JComponent>();
         //TODO can we fill these labels from the properties file?
+        // see: https://stackoverflow.com/questions/1380343/parse-map-from-properties-files
+        // then use: speedSlider.labelTable=1,Faster;50,Slower
+//        String labelTable = getBundleString("speedSlider.labelTable");
+//        LOG.info("labelTable:"+labelTable);
         labels.put(speedSlider.getMinimum(), new JLabel("Faster"));
         labels.put(speedSlider.getMaximum(), new JLabel("Slower"));
         speedSlider.setLabelTable(labels);
@@ -195,6 +277,29 @@ trailSlider.value=3
         controls.add(trailSlider);
 
         return controls;
+    }
+
+    // wie in ButtonDemo:
+    Border loweredBorder = new CompoundBorder(new SoftBevelBorder(SoftBevelBorder.LOWERED), new EmptyBorder(5,5,5,5));
+    private JXPanel createHorizontalPanel(boolean threeD) {
+        JXPanel p = new JXPanel();
+        p.setLayout(new BoxLayout(p, BoxLayout.X_AXIS));
+        p.setAlignmentY(Component.TOP_ALIGNMENT);
+        p.setAlignmentX(Component.LEFT_ALIGNMENT);
+        if(threeD) {
+            p.setBorder(loweredBorder);
+        }
+        return p;
+    }
+    private JXPanel createVerticalPanel(boolean threeD) {
+        JXPanel p = new JXPanel();
+        p.setLayout(new BoxLayout(p, BoxLayout.Y_AXIS));
+        p.setAlignmentY(Component.TOP_ALIGNMENT);
+        p.setAlignmentX(Component.LEFT_ALIGNMENT);
+        if(threeD) {
+            p.setBorder(loweredBorder);
+        }
+        return p;
     }
 
 }
