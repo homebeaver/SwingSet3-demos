1cafa74bb6c0d113a3f27ee221f389ca9fe8d657
homebeaver <homebeaver@outlook.de>
2022-08-28 23:16:00 +0200

    method private JComponent createRightFB(
    PromptSupport.FocusBehavior focusBehavior, Locale l) {

--- src/main/java/org/jdesktop/swingx/demos/formattedtext/FormattedTextDemo.java
+++ src/main/java/org/jdesktop/swingx/demos/formattedtext/FormattedTextDemo.java
@@ -4,7 +4,6 @@ import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Font;
 import java.awt.Frame;
-import java.awt.event.ActionEvent;
 import java.text.DecimalFormat;
 import java.text.NumberFormat;
 import java.text.ParseException;
@@ -13,17 +12,15 @@ import java.util.Date;
 import java.util.Locale;
 import java.util.logging.Logger;
 
-import javax.swing.AbstractAction;
-import javax.swing.JButton;
 import javax.swing.JComponent;
 import javax.swing.JFormattedTextField;
+import javax.swing.JFormattedTextField.AbstractFormatter;
 import javax.swing.JLabel;
 import javax.swing.JSplitPane;
 import javax.swing.SwingConstants;
 import javax.swing.SwingUtilities;
 import javax.swing.border.BevelBorder;
-import javax.swing.event.DocumentEvent;
-import javax.swing.event.DocumentListener;
+import javax.swing.text.DateFormatter;
 
 import org.jdesktop.swingx.JXFormattedTextField;
 import org.jdesktop.swingx.JXFrame;
@@ -75,48 +72,9 @@ public class FormattedTextDemo extends AbstractDemo {
     JXPanel right;
     JXFormattedTextField tfDatum;
     JXFormattedTextField tfBetrag;
+    double betrag = 0;
     
     NumberFormat nf = null;
-    private Number parse(String source) {
-    	if(nf==null) {
-        	nf = NumberFormat.getNumberInstance();
-    	}
-    	try {
-    		DecimalFormat df = (DecimalFormat)nf;
-    		df.setParseIntegerOnly(false);
-    		df.setParseBigDecimal(true);
-    		Number num = df.parse(source);
-        	LOG.info("result "+num.getClass() +":"+num + " :"+source); // TODO BUG
-        	// wg. Locale DE wird "22,3" zu 22.3 geparst, was korrekt ist, ABER "22.3" als Long 223 ist ein BUG
-    	} catch (ParseException pe) {
-    		LOG.warning(pe.getMessage()+" at "+pe.getErrorOffset());
-    	}
-    	return null;
-    }
-    
-    private Number parseXXX(String source) {
-    	if(nf==null) {
-        	nf = NumberFormat.getNumberInstance();
-//        	nf = NumberFormat.getCurrencyInstance(); // TODO parst nichts !!!!
-//        	This is equivalent to calling 
-//        	getCurrencyInstance(Locale.getDefault(Locale.Category.FORMAT));
-        	LOG.info("+++current value of the default locale:"+Locale.getDefault(Locale.Category.FORMAT)
-        		+ " CurrencyInstance="+nf + " Currency="+nf.getCurrency()
-        	);     	
-    	}
-//    	try {
-    		DecimalFormat df = (DecimalFormat)nf;
-    		df.setParseIntegerOnly(false);
-    		//df.setMaximumFractionDigits(3);
-    		ParsePosition pp = new ParsePosition(0);
-    		Number num = df.parse(source, pp);
-//    		Number num = df.parse(source);
-        	LOG.info("result Number:"+num + " ParsePosition="+pp + " :"+source);
-//    	} catch (ParseException pe) {
-//    		LOG.warning(pe.getMessage()+" at "+pe.getErrorOffset());
-//    	}
-    	return null;
-    }
 
     /**
      * Constructor
@@ -140,44 +98,7 @@ public class FormattedTextDemo extends AbstractDemo {
 
     @Override
 	public JXPanel getControlPane() {
-//		return emptyControlPane();
-    	JXPanel panel = new JXPanel(new BorderLayout());
-    	
-        JButton foregroundColor = new JButton(new AbstractAction("Open as a dialog") {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-            	LOG.info("ActionEvent e:"+e);
-//            	JXPanel panel = new JXPanel();
-//            	panel.add(tfBetrag);
-//            	tfBetrag.setValue("nix");
-//            	PromptSupport.init("promptText", Color.YELLOW, Color.BLACK, tfBetrag);
-//            	PromptSupport.setFontStyle(Font.BOLD+Font.ITALIC, tfBetrag);
-            	tfBetrag.setPromptFontStyle(Font.ITALIC);
-            	tfBetrag.addBuddy(new JLabel("rechts"), Position.RIGHT);
-            	LOG.info("??? tfBetrag.getLayout():"+tfBetrag.getLayout());
-//            	tfBetrag.getLayout().getClass() muss BuddyLayoutAndBorder sein
-//                Color color = JColorChooser.showDialog(PromptSupportDemo.this, "Foreground Color", PromptSupport.getForeground(textField));
-//                
-//                if (color != null) {
-//                    PromptSupport.setForeground(color, textField);
-//                    JXTipOfTheDay dialog = new JXTipOfTheDay(model);
-//                    dialog.setCurrentTip(0);
-//                    dialog.showDialog(totd);
-//               }
-//            	panel.getUI().c.createDialog(parentComponent, choice);
-            	//panel.show ???
-            	
-            	tfDatum.setPrompt("Datum");
-            	tfDatum.setForeground(Color.BLUE);
-            	tfDatum.setBackground(Color.ORANGE);
-            	tfDatum.addBuddy(new JLabel("links"), Position.LEFT);
-            	
-//            	right.revalidate();
-            }
-        });
-        panel.add(foregroundColor);
-
-    	return panel;
+		return emptyControlPane();
 	}
 
     private JComponent createRightPane() {
@@ -188,15 +109,16 @@ public class FormattedTextDemo extends AbstractDemo {
         sep.setTitle("JXFormattedTextField:");
         sep.setHorizontalAlignment(SwingConstants.CENTER);
         right.add(sep);
+        
         sep = new JXTitledSeparator();
         sep.setTitle("HIGHLIGHT_PROMPT");
     	/* FocusBehavior:
     	 * Determines how the {@link JTextComponent} is rendered 
     	 * when focused and no text is present.
     	 */
-       right.add(sep);
+        right.add(sep);
 
-        JXPanel rightHP = new JXPanel();
+        JXPanel rightHP = new JXPanel(); // HP == HIGHLIGHT_PROMPT
         right.add(rightHP);
         
 		JLabel labelDatum = new JLabel("Datum");
@@ -232,14 +154,15 @@ public class FormattedTextDemo extends AbstractDemo {
 //		tfBetrag.setOuterMargin(Insets margin)
 		LOG.info("tfBetrag.getFocusBehavior()="+tfBetrag.getFocusBehavior());
 		
-		NumberFormat betrag = NumberFormat.getCurrencyInstance();
+//		NumberFormat betrag = NumberFormat.getCurrencyInstance();
 //		if(betrag instanceof DecimalFormat) {
 //			DecimalFormat df = (DecimalFormat)betrag; // DecimalFormat extends NumberFormat
 //			LOG.info("DecimalFormat betrag:"+df.format(0l)); // 0,00 €	
 //			tfBetrag.setValue(df.format(0l)); 
 //		}
-		LOG.info("betrag:"+betrag.format(4.3f)); // 4,30 €
+//		LOG.info("betrag:"+betrag.format(4.3f)); // 4,30 €
 //		tfBetrag.setValue(betrag); // exception Cannot format given Object as a Number
+		tfBetrag.setValue(betrag);
 /*
 Exception in thread "AWT-EventQueue-0" java.lang.IllegalArgumentException: Cannot format given Object as a Number
 	at java.base/java.text.DecimalFormat.format(DecimalFormat.java:515)
@@ -259,109 +182,154 @@ Exception in thread "AWT-EventQueue-0" java.lang.IllegalArgumentException: Canno
 		tfBetrag.setColumns(10);
 		tfBetrag.addBuddy(new JLabel("links"), Position.LEFT);
 		tfBetrag.addBuddy(new JLabel("€"), Position.RIGHT);
-//		tfBetrag.addActionListener(ae -> {
-//			LOG.info("ActionEvent "+ae);
-//		});
-//		tfBetrag.addActionListener(new ActionListener() {
-//            public void actionPerformed(ActionEvent ae) {
-//            	LOG.info("ActionEvent "+ae);
-//                String  value = ((JTextField)ae.getSource()).getText();
-//                int newSize;
-//
-////                try {
-////                    newSize = Integer.parseInt(value);
-////                } catch (Exception ex) {
-////                    newSize = -1;
-////                }
-////                if(newSize > 0) {
-////                    splitPane.setDividerSize(newSize);
-////                    setMinimumSize();
-////                } else {
-////                    JOptionPane.showMessageDialog(splitPane,
-////                                                  getBundleString("invalid_divider_size"),
-////                                                  getBundleString("error"),
-////                                                  JOptionPane.ERROR_MESSAGE);
-////                }
-//            }
-//        });
-
-/*
-
-The following code snippetshows a potential implementation of such an InputVerifier: 
- public class FormattedTextFieldVerifier extends InputVerifier {
-     public boolean verify(JComponent input) {
-         if (input instanceof JFormattedTextField) {
-             JFormattedTextField ftf = (JFormattedTextField)input;
-             AbstractFormatter formatter = ftf.getFormatter();
-             if (formatter != null) {
-                 String text = ftf.getText();
-                 try {
-                      formatter.stringToValue(text);
-                      return true;
-                  } catch (ParseException pe) {
-                      return false;
-                  }
-              }
-          }
-          return true;
-      }
-      public boolean shouldYieldFocus(JComponent input) {
-          return verify(input);
-      }
-  }
-
-
-
- */
-		tfBetrag.getDocument().addDocumentListener(new DocumentListener() {
-            @Override
-            public void removeUpdate(DocumentEvent e) {
-            	String text = tfBetrag.getText();
-            	LOG.info("DocumentEvent "+e + " :"+text);
-            	parse(text);
-            }
-            
-            @Override
-            // there was an insert into the document
-            public void insertUpdate(DocumentEvent e) {
-//            	DecimalFormat df;
-//            	NumberFormat nf = NumberFormat.getCurrencyInstance();
-////            	This is equivalent to calling 
-////            	getCurrencyInstance(Locale.getDefault(Locale.Category.FORMAT)).
-//            	LOG.info(" current value of the default locale:"+Locale.getDefault(Locale.Category.FORMAT));
-////            	AbstractFormatter formatter = tfBetrag.getFormatter();
-//            	String text = tfBetrag.getText();
-//            	LOG.info("DocumentEvent "+e + " :"+text);
-//            	try {
-//            		Number num = nf.parse(text);
-//            		//nf.setParseIntegerOnly(false);
-//            		//nf.setMaximumFractionDigits(3);
-////            		Object o = formatter.stringToValue(text);
-//                	LOG.info("DocumentEvent Number:"+num + " :"+text);
-//            	} catch (ParseException pe) {
-//            		LOG.warning(pe.getMessage()+" at "+pe.getErrorOffset());
-//            	}
-            	String text = tfBetrag.getText();
-            	LOG.info("DocumentEvent "+e + " :"+text);
-            	parse(text);
-            }
-            
-            @Override
-            // attribute or set of attributes changed
-            public void changedUpdate(DocumentEvent e) {
-            	String text = tfBetrag.getText();
-            	LOG.info("DocumentEvent "+e + " :"+text);
-            	parse(text);
-            }
+		tfBetrag.addPropertyChangeListener(pce -> {
+        	LOG.info("tfBetrag PropertyChangeEvent:"+pce);
+        	String s =tfBetrag.getText(); // String javax.swing.text.JTextComponent.getText()
+        	Object o = tfBetrag.getValue(); // Object javax.swing.JFormattedTextField.getValue()
+        	LOG.info("tfBetrag Value:"+o + " String:"+s);
+        	if(nf==null) {
+//        		nf = NumberFormat.getNumberInstance();
+        		nf = NumberFormat.getCurrencyInstance();
+        	}
+    		DecimalFormat df = (DecimalFormat)nf;
+        	if(s.length()>0) try {
+				Number n = df.parse(s);
+				LOG.info("tfBetrag Number:"+n + " String:"+s);
+//	    		Number num = df.parse(source);
+//	        	LOG.info("result "+num.getClass() +":"+num + " :"+source); // TODO BUG, denn
+//	        	// wg. Locale DE wird "22,3" zu 22.3 geparst, was korrekt ist, 
+				// ABER "22.3" als Long 223 ist ein BUG
+				tfBetrag.setValue(n);
+			} catch (ParseException e) {
+				int eo = e.getErrorOffset();
+				LOG.warning("ErrorOffset="+eo);
+				e.printStackTrace();
+				tfBetrag.setValue(0);
+				if(eo>0) {
+					df = (DecimalFormat)NumberFormat.getNumberInstance();
+					Number num = df.parse(s.substring(0, eo), new ParsePosition(0));
+					LOG.info("result "+num.getClass() +":"+num + " :"+s);
+					tfBetrag.setValue(num);
+				}
+			}
         });
-
 
 		rightHP.add(labelDatum);
 		rightHP.add(tfDatum);
 		rightHP.add(labelBetrag);
 		rightHP.add(tfBetrag);
-
+       
+        sep = new JXTitledSeparator();
+        sep.setTitle("SHOW_PROMPT");
+        right.add(sep);
+        // SHOW_PROMPT : Keep the prompt text visible:
+        right.add(createRightFB(PromptSupport.FocusBehavior.SHOW_PROMPT, Locale.US));
+        
+        sep = new JXTitledSeparator();
+        sep.setTitle("HIDE_PROMPT");
+        right.add(sep);
+        // HIDE_PROMPT : Hide the prompt text: TODO BUG, denn Prompt wird gezeigt
+        right.add(createRightFB(PromptSupport.FocusBehavior.HIDE_PROMPT, Locale.UK));
+        
         return right;
+    }
+    
+    private JComponent createRightFB(PromptSupport.FocusBehavior focusBehavior, Locale l) {
+        JXPanel panel = new JXPanel();
+
+		String localcurrencysymbol = "$";
+		String localcurrencycode = "USD";
+
+		JLabel labelDatum = new JLabel("Datum");
+		JLabel labelBetrag = new JLabel("Betrag");
+
+		JXFormattedTextField tfDatum = new JXFormattedTextField(new Date()); // heute
+		tfDatum.setFocusBehavior(focusBehavior);
+		tfDatum.setColumns(12);
+		tfDatum.setPrompt("Datum hier eingeben");
+		tfDatum.setPromptForeground(Color.BLUE);
+		tfDatum.setPromptBackground(Color.ORANGE);
+		tfDatum.setPromptFontStyle(Font.BOLD+Font.ITALIC);
+		tfDatum.setValue(null);
+		tfDatum.addPropertyChangeListener(pce -> {
+        	LOG.info("tfDatum PropertyChangeEvent:"+pce);
+        	
+        	/* String javax.swing.text.JTextComponent.getText()
+        	 * Returns the text contained in this TextComponent.
+        	 * If the underlying document is null, will give a NullPointerException.
+        	 * Note that text is not a bound property, so no PropertyChangeEvent is fired when it changes. 
+        	 * To listen for changes to the text, use DocumentListener.
+        	 */
+        	String s = tfDatum.getText();
+        	
+        	/* Object javax.swing.JFormattedTextField.getValue()
+        	 * Returns the last valid value. 
+        	 * Based on the editing policy oft he AbstractFormatter this may not return the current value. 
+        	 * The currently edited value can be obtained by invoking commitEdit followed by getValue.
+        	 */
+        	Object o = tfDatum.getValue();
+        	LOG.info("tfDatum Value:"+o + ", Text String:"+s + (s.isEmpty() ? "(empty) " : ", ") 
+        			+ "formatter:"+tfDatum.getFormatter());
+        	
+        	if(!s.isEmpty()) {
+        		AbstractFormatter f = tfDatum.getFormatter(); // TextFormatter, responsible for formatting the current value
+        		DateFormatter df = (DateFormatter)f;
+        		try {
+					Object datum = df.stringToValue(s);
+				} catch (ParseException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+        	}
+		});
+
+		NumberFormat numberFormat = NumberFormat.getCurrencyInstance(l);
+        if(numberFormat instanceof java.text.DecimalFormat df) {
+        	localcurrencysymbol = df.getCurrency().getSymbol();
+        	localcurrencycode = df.getCurrency().getCurrencyCode();
+        	//df.getCurrency().getDisplayName()
+        	LOG.info("localcurrencysymbol/code="+localcurrencysymbol+"7"+localcurrencycode);
+        }
+		JXFormattedTextField tfBetrag = new JXFormattedTextField(numberFormat);
+		tfBetrag.setFocusBehavior(focusBehavior);
+		tfBetrag.setColumns(12);
+		tfBetrag.setPrompt(localcurrencycode+" Betrag");
+		tfBetrag.setPromptForeground(Color.BLUE);
+		tfBetrag.setPromptBackground(Color.ORANGE);
+		tfBetrag.setPromptFontStyle(Font.BOLD+Font.ITALIC);
+		tfBetrag.addBuddy(new JLabel(localcurrencysymbol), Position.RIGHT);
+		tfBetrag.addPropertyChangeListener(pce -> {
+        	String s = tfBetrag.getText();
+        	Object o = tfBetrag.getValue();
+        	LOG.info("tfBetrag Value:"+o + ", Text String:"+s + (s.isEmpty() ? "(empty) " : ", ") 
+        			+ "formatter:"+tfBetrag.getFormatter());
+        	if(!s.isEmpty()) {
+        		DecimalFormat df = (DecimalFormat)numberFormat;
+        		try {
+					tfBetrag.setValue(df.parse(s));
+				} catch (ParseException e) {
+					int eo = e.getErrorOffset();
+					LOG.warning(e.getMessage() + ", ErrorOffset="+eo);
+//					e.printStackTrace();
+					tfBetrag.setValue(0); // fallback
+					tfBetrag.removeAllBuddies();
+					if(eo>0) {
+						df = (DecimalFormat)NumberFormat.getNumberInstance();
+						Number num = df.parse(s.substring(0, eo), new ParsePosition(0));
+						LOG.info("result "+num.getClass() +":"+num + " :"+s);
+						tfBetrag.setValue(num);
+					}
+				}
+        	}
+			
+		});
+
+		panel.add(labelDatum);
+		panel.add(tfDatum);
+		panel.add(labelBetrag);
+		panel.add(tfBetrag);
+
+		return panel;
     }
 
 	/*
