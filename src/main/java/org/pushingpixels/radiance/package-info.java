b6eab29b80451c7d7528d55bdb88a1b9e07a16da
homebeaver <homebeaver@outlook.de>
2022-09-08 22:09:53 +0200

    package + sub kopiert von radiance branch sunshine 5.0

---
+++ src/main/java/org/pushingpixels/radiance/package-info.java
@@ -0,0 +1,7 @@
+/*
+ * EUG: kopiert von radiance branch sunshine
+ * 5.0 - **Heliotrope** (October 25, 2021), Java 9+. 
+ * Details in the [announcement post](https://www.pushing-pixels.org/2021/10/29/radiance-5-0-0.html).
+ */
+package org.pushingpixels.radiance
+;
