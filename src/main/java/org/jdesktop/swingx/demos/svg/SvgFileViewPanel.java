b38c7c00d5cc7cf5764f2a50f700300330f6119e
homebeaver <homebeaver@outlook.de>
2022-09-18 19:47:37 +0200

    transcodeToJava + dynCompile

--- src/main/java/org/jdesktop/swingx/demos/svg/SvgFileViewPanel.java
+++ src/main/java/org/jdesktop/swingx/demos/svg/SvgFileViewPanel.java
@@ -30,6 +30,56 @@
 // original: package org.pushingpixels.radiance.demo.component.svg;
 package org.jdesktop.swingx.demos.svg;
 
+import static java.lang.StackWalker.Option.RETAIN_CLASS_REFERENCE;
+
+import java.awt.Dimension;
+import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.lang.invoke.MethodHandles;
+import java.lang.invoke.MethodHandles.Lookup;
+import java.lang.reflect.Method;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.net.URLClassLoader;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Deque;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.logging.Logger;
+import java.util.stream.Collectors;
+
+import javax.annotation.processing.Processor;
+import javax.swing.JOptionPane;
+import javax.swing.SwingUtilities;
+import javax.swing.SwingWorker;
+import javax.tools.FileObject;
+import javax.tools.ForwardingJavaFileManager;
+import javax.tools.JavaCompiler;
+import javax.tools.JavaCompiler.CompilationTask;
+import javax.tools.JavaFileManager;
+import javax.tools.SimpleJavaFileObject;
+import javax.tools.StandardJavaFileManager;
+import javax.tools.ToolProvider;
+
+import org.pushingpixels.radiance.common.api.RadianceCommonCortex;
+import org.pushingpixels.radiance.common.api.icon.RadianceIcon;
 import org.pushingpixels.radiance.component.api.bcb.BreadcrumbBarCallBack;
 import org.pushingpixels.radiance.component.api.common.CommandButtonPresentationState;
 import org.pushingpixels.radiance.component.api.common.JCommandButtonPanel;
@@ -41,22 +91,8 @@ import org.pushingpixels.radiance.component.api.common.model.CommandGroup;
 import org.pushingpixels.radiance.component.api.common.model.CommandPanelContentModel;
 import org.pushingpixels.radiance.component.api.common.model.CommandPanelPresentationModel;
 import org.pushingpixels.radiance.component.api.common.projection.CommandPanelProjection;
-import org.pushingpixels.radiance.common.api.RadianceCommonCortex;
 import org.pushingpixels.radiance.tools.svgtranscoder.api.SvgStreamTranscoder;
 import org.pushingpixels.radiance.tools.svgtranscoder.api.java.JavaLanguageRenderer;
-import org.pushingpixels.radiance.common.api.icon.RadianceIcon;
-
-import javax.swing.*;
-import java.awt.*;
-import java.io.ByteArrayInputStream;
-import java.io.File;
-import java.io.InputStream;
-import java.io.PrintWriter;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.logging.Logger;
 
 /**
  * Panel that hosts SVG-based gallery buttons.
@@ -130,6 +166,323 @@ public class SvgFileViewPanel extends JCommandButtonPanel {
         this.callback = callback;
     }
 
+    private Class<?> transcodeToJava(String name, SvgBatikRadianceIcon svgIcon) throws FileNotFoundException {
+        String svgClassName = name.substring(0, name.lastIndexOf('.'));
+        svgClassName = svgClassName.replace('-', '_');
+        // siehe [#74] unten
+        svgClassName = "TMP"+svgClassName.replace(' ', '_');
+
+        String javaClassFilename = System.getProperty("java.io.tmpdir")
+                + File.separator + svgClassName + ".java";
+        LOG.info("transcode "+name+" to "+javaClassFilename);
+        PrintWriter pw = new PrintWriter(javaClassFilename); // throws FileNotFoundException
+
+        SvgStreamTranscoder transcoder = new SvgStreamTranscoder(
+                new ByteArrayInputStream(svgIcon.getSvgBytes()), svgClassName,
+                new JavaLanguageRenderer());
+        String packageName = "org.jdesktop.swingx.demos.svg";
+        transcoder.setPackageName(packageName);
+        transcoder.setPrintWriter(pw);
+        transcoder.transcode(this.getClass().getResourceAsStream(
+                "/org/pushingpixels/radiance/tools/svgtranscoder/api/java/" + TRANSCODER_TEMPLATE));
+        JOptionPane.showMessageDialog(
+                SwingUtilities.getWindowAncestor(SvgFileViewPanel.this),
+                "Finished with '" + javaClassFilename + "'");
+    	// der SvgStreamTranscoder kann nur in PrintWriter schreiben, also müssen wir vom file lesen
+        // TODO in stream schreiben als pipe lesen
+        String className = packageName+"."+svgClassName;
+        File inFile = new File(javaClassFilename);
+        InputStream inStream = new FileInputStream(inFile);
+        Class<?> compiledClass = dynCompile(inStream, className);
+        return compiledClass;
+    }
+	private Class<?> dynCompile(InputStream inSrc, String className) {
+		JavaCompiler compiler = ToolProvider.getSystemJavaCompiler();
+		// https://dzone.com/articles/how-to-compile-a-class-at-runtime-with-java-8-and
+		if (compiler == null)
+			throw new RuntimeException("No compiler was provided by ToolProvider.getSystemJavaCompiler()."
+					+ "Make sure the jdk.compiler module is available.");
+		ClassFileManager fileManager = new ClassFileManager(compiler.getStandardFileManager(null, null, null));
+		Lookup lookup = MethodHandles.lookup();
+		ClassLoader cl = lookup.lookupClass().getClassLoader();
+		List<CharSequenceJavaFileObject> files = new ArrayList<>();
+		LOG.info("compile className " + className);
+		
+		String content = new BufferedReader(new InputStreamReader(inSrc, StandardCharsets.UTF_8))
+				.lines()
+				.collect(Collectors.joining("\n"));
+		LOG.info("content : " + content);
+		files.add(new CharSequenceJavaFileObject(className, content));
+		
+		StringWriter out = new StringWriter();
+		CompileOptions compileOptions = new CompileOptions();
+		List<String> options = new ArrayList<>(compileOptions.options);
+		if (!options.contains("-classpath")) {
+			StringBuilder classpath = new StringBuilder();
+			String separator = System.getProperty("path.separator");
+			String cp = System.getProperty("java.class.path");
+			String mp = System.getProperty("jdk.module.path");
+
+			if (cp != null && !"".equals(cp))
+				classpath.append(cp);
+			if (mp != null && !"".equals(mp))
+				classpath.append(mp);
+
+			if (cl instanceof URLClassLoader) {
+				for (URL url : ((URLClassLoader) cl).getURLs()) {
+					if (classpath.length() > 0)
+						classpath.append(separator);
+
+					if ("file".equals(url.getProtocol()))
+						try {
+							classpath.append(new File(url.toURI()));
+						} catch (URISyntaxException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						}
+				}
+			}
+
+			options.addAll(Arrays.asList("-classpath", classpath.toString()));
+		}
+	
+		CompilationTask task = compiler.getTask(out, fileManager, null, options, null, files);
+		if (!compileOptions.processors.isEmpty())
+			task.setProcessors(compileOptions.processors);
+		task.call();
+		boolean expectResult = true;
+		if (fileManager.isEmpty()) {
+			if (!expectResult) {
+				LOG.info("expectResult == false ==> return null");
+				return null;
+			}
+			throw new RuntimeException("Compilation error: " + out);
+		}
+		LOG.info("Compilation OK fileManager:" + fileManager.toString());
+
+		Class<?> result = null;
+		Class<?> caller = StackWalker
+				.getInstance(RETAIN_CLASS_REFERENCE)
+				.walk(s -> s
+						.skip(2)
+						.findFirst()
+						.get()
+						.getDeclaringClass());
+
+		// If the compiled class is in the same package as the caller class,
+		// then we can use the private-access Lookup of the caller class
+		if (className.startsWith(caller.getPackageName() + ".")
+	            // [#74] This heuristic is necessary to prevent classes in subpackages of the caller to be loaded
+	            //       this way, as subpackages cannot access private content in super packages.
+	            //       The heuristic will work only with classes that follow standard naming conventions.
+	            //       A better implementation is difficult at this point.
+				&& Character.isUpperCase(className.charAt(caller.getPackageName().length() + 1))) {
+			LOG.info("compiled class is in the same package as the caller class");
+			try {
+				Lookup privateLookup = MethodHandles.privateLookupIn(caller, lookup);
+				result = fileManager.loadAndReturnMainClass(className, 
+						(name, bytes) -> privateLookup.defineClass(bytes)
+					);
+				LOG.info("result class:"+result);
+			} catch (IllegalAccessException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+
+		// Otherwise, use an arbitrary class loader. This approach doesn't allow for
+		// loading private-access interfaces in the compiled class's type hierarchy
+		else {
+			LOG.info("compiled class is in different package as the caller class");
+			ByteArrayClassLoader c = new ByteArrayClassLoader(fileManager.classes());
+			try {
+				result = fileManager.loadAndReturnMainClass(className, (name, bytes) -> c.loadClass(name));
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		return result;
+	}
+
+	// aus https://github.com/jOOQ/jOOR
+	static final class ByteArrayClassLoader extends ClassLoader {
+		private final Map<String, byte[]> classes;
+
+		ByteArrayClassLoader(Map<String, byte[]> classes) {
+			super(ByteArrayClassLoader.class.getClassLoader());
+			this.classes = classes;
+		}
+
+		@Override
+		protected Class<?> findClass(String name) throws ClassNotFoundException {
+			byte[] bytes = classes.get(name);
+			if (bytes == null)
+				return super.findClass(name);
+			else
+				return defineClass(name, bytes, 0, bytes.length);
+		}
+	}
+
+    static final class JavaFileObject extends SimpleJavaFileObject {
+        final ByteArrayOutputStream os = new ByteArrayOutputStream();
+
+        JavaFileObject(String name, JavaFileObject.Kind kind) {
+            super(URI.create("string:///" + name.replace('.', '/') + kind.extension), kind);
+        }
+
+        byte[] getBytes() {
+            return os.toByteArray();
+        }
+
+        @Override
+        public OutputStream openOutputStream() {
+            return os;
+        }
+
+        @Override
+        public CharSequence getCharContent(boolean ignoreEncodingErrors) {
+            return new String(os.toByteArray(), StandardCharsets.UTF_8);
+        }
+    }
+
+    @FunctionalInterface
+    interface ThrowingBiFunction<T, U, R> {
+        R apply(T t, U u) throws Exception;
+    }
+
+    static final class CharSequenceJavaFileObject extends SimpleJavaFileObject {
+        final CharSequence content;
+
+        public CharSequenceJavaFileObject(String className, CharSequence content) {
+            super(URI.create("string:///" + className.replace('.', '/') + JavaFileObject.Kind.SOURCE.extension), JavaFileObject.Kind.SOURCE);
+            this.content = content;
+        }
+
+        @Override
+        public CharSequence getCharContent(boolean ignoreEncodingErrors) {
+            return content;
+        }
+    }
+
+    static final class ClassFileManager extends ForwardingJavaFileManager<StandardJavaFileManager> {
+        private final Map<String, JavaFileObject> fileObjectMap;
+        private Map<String, byte[]> classes;
+
+        ClassFileManager(StandardJavaFileManager standardManager) {
+            super(standardManager);
+
+            fileObjectMap = new LinkedHashMap<>();
+        }
+
+        @Override
+        public JavaFileObject getJavaFileForOutput(
+            JavaFileManager.Location location,
+            String className,
+            JavaFileObject.Kind kind,
+            FileObject sibling
+        ) {
+            JavaFileObject result = new JavaFileObject(className, kind);
+            fileObjectMap.put(className, result);
+            return result;
+        }
+
+        boolean isEmpty() {
+            return fileObjectMap.isEmpty();
+        }
+
+        Map<String, byte[]> classes() {
+            if (classes == null) {
+                classes = new LinkedHashMap<>();
+
+                for (Entry<String, JavaFileObject> entry : fileObjectMap.entrySet())
+                    classes.put(entry.getKey(), entry.getValue().getBytes());
+            }
+
+            return classes;
+        }
+
+        Class<?> loadAndReturnMainClass(String mainClassName, ThrowingBiFunction<String, byte[], Class<?>> definer) throws Exception {
+            Class<?> result = null;
+
+            // [#117] We don't know the subclass hierarchy of the top level
+            //        classes in the compilation unit, and we can't find out
+            //        without either:
+            //
+            //        - class loading them (which fails due to NoClassDefFoundError)
+            //        - using a library like ASM (which is a big and painful dependency)
+            //
+            //        Simple workaround: try until it works, in O(n^2), where n
+            //        can be reasonably expected to be small.
+            Deque<Entry<String, byte[]>> queue = new ArrayDeque<>(classes().entrySet());
+            int n1 = queue.size();
+
+            // Try at most n times
+            for (int i1 = 0; i1 < n1 && !queue.isEmpty(); i1++) {
+                int n2 = queue.size();
+
+                for (int i2 = 0; i2 < n2; i2++) {
+                    Entry<String, byte[]> entry = queue.pop();
+
+                    try {
+                        Class<?> c = definer.apply(entry.getKey(), entry.getValue());
+
+                        if (mainClassName.equals(entry.getKey()))
+                            result = c;
+                    }
+                    // public class ReflectException extends RuntimeException {
+                    catch (RuntimeException e) {
+                        queue.offer(entry);
+                    }
+                }
+            }
+
+            return result;
+        }
+    }
+    
+	public final class CompileOptions {
+
+		final List<? extends Processor> processors;
+		final List<String> options;
+
+		public CompileOptions() {
+			this(Collections.emptyList(), Collections.emptyList());
+		}
+
+		private CompileOptions(List<? extends Processor> processors, List<String> options) {
+			this.processors = processors;
+			this.options = options;
+		}
+
+		public final CompileOptions processors(Processor... newProcessors) {
+			return processors(Arrays.asList(newProcessors));
+		}
+
+		public final CompileOptions processors(List<? extends Processor> newProcessors) {
+			return new CompileOptions(newProcessors, options);
+		}
+
+		public final CompileOptions options(String... newOptions) {
+			return options(Arrays.asList(newOptions));
+		}
+
+		public final CompileOptions options(List<String> newOptions) {
+			return new CompileOptions(processors, newOptions);
+		}
+
+		final boolean hasOption(String opt) {
+			for (String option : options)
+				if (option.equalsIgnoreCase(opt))
+					return true;
+
+			return false;
+		}
+	}
+
     /**
      * Sets the current files to show. The current contents of the panel are discarded. The file
      * list is scanned for files ending with <code>.svg</code> or <code>.svgz</code>. For each such
@@ -146,7 +499,7 @@ public class SvgFileViewPanel extends JCommandButtonPanel {
 
         /*
 
-newCommands key : icon name , value : transcode-command / transcode svg tp java
+newCommands key : icon name , value : transcode-command / transcode svg to java
 
 die icons zum "icon name"s werden in mainWorker.process gebildet
 
@@ -164,35 +517,19 @@ die icons zum "icon name"s werden in mainWorker.process gebildet
                     .setText(name.replace('-', ' '))
                     .setIconFactory(EmptyRadianceIcon.factory())
                     .setAction(commandActionEvent -> {
-                    	// transcode svg to java
+                    	// transcode svg to java and compile it
                         try {
                             RadianceIcon icon = commandActionEvent.getCommand().getIconFactory().createNewIcon();
                             if (!(icon instanceof SvgBatikRadianceIcon)) {
                                 return;
                             }
-
                             SvgBatikRadianceIcon svgIcon = (SvgBatikRadianceIcon) icon;
-
-                            System.out.println(name);
-                            String svgClassName = name.substring(0, name.lastIndexOf('.'));
-                            svgClassName = svgClassName.replace('-', '_');
-                            svgClassName = svgClassName.replace(' ', '_');
-
-                            String javaClassFilename = System.getProperty("java.io.tmpdir")
-                                    + File.separator + svgClassName + ".java";
-                            LOG.info("transcode to "+javaClassFilename);
-                            PrintWriter pw = new PrintWriter(javaClassFilename);
-
-                            SvgStreamTranscoder transcoder = new SvgStreamTranscoder(
-                                    new ByteArrayInputStream(svgIcon.getSvgBytes()), svgClassName,
-                                    new JavaLanguageRenderer());
-
-                            transcoder.setPrintWriter(pw);
-                            transcoder.transcode(this.getClass().getResourceAsStream(
-                                    "/org/pushingpixels/radiance/tools/svgtranscoder/api/java/" + TRANSCODER_TEMPLATE));
-                            JOptionPane.showMessageDialog(
-                                    SwingUtilities.getWindowAncestor(SvgFileViewPanel.this),
-                                    "Finished with '" + javaClassFilename + "'");
+                            Class<?> compiledIcon = transcodeToJava(name, svgIcon);
+                            Method method = compiledIcon.getMethod("factory", null);
+                            RadianceIcon.Factory f = (RadianceIcon.Factory) method.invoke(null);
+                            RadianceIcon ri = f.createNewIcon();
+                            LOG.info("compiled RadianceIcon.Factory:"+ri);
+                            LOG.info("compiled RadianceIcon IconHeight:"+ri.getIconHeight());
                         } catch (Throwable exc) {
                             exc.printStackTrace();
                         }
