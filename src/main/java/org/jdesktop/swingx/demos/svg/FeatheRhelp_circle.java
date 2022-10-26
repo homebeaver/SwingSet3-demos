a2bf56227ddb8d4dd58a964dfa3d78aa071bf604
homebeaver <homebeaver@outlook.de>
2022-10-26 16:20:57 +0200

    add ErrorMessageButton, use alternative icons

---
+++ src/main/java/org/jdesktop/swingx/demos/svg/FeatheRhelp_circle.java
@@ -0,0 +1,273 @@
+package org.jdesktop.swingx.demos.svg;
+
+import java.awt.*;
+import java.awt.geom.*;
+import java.util.Stack;
+import javax.swing.plaf.UIResource;
+
+import org.jdesktop.swingx.icon.RadianceIcon;
+import org.jdesktop.swingx.icon.RadianceIconUIResource;
+
+/**
+ * This class has been automatically generated using 
+ * <a href="https://jdesktop.wordpress.com/2022/09/25/svg-icons/">Radiance SVG converter</a>.
+ */
+public class FeatheRhelp_circle implements RadianceIcon {
+    private Shape shape = null;
+    private GeneralPath generalPath = null;
+    private Paint paint = null;
+    private Stroke stroke = null;
+    private RadianceIcon.ColorFilter colorFilter = null;
+    private Stack<AffineTransform> transformsStack = new Stack<>();
+
+	// EUG https://github.com/homebeaver (rotation + point/axis reflection)
+    private int rsfx = 1, rsfy = 1;
+    public void setReflection(boolean horizontal, boolean vertical) {
+    	this.rsfx = vertical ? -1 : 1;
+    	this.rsfy = horizontal ? -1 : 1;
+    }    
+    public boolean isReflection() {
+		return rsfx==-1 || rsfy==-1;
+	}
+	
+    private double theta = 0;
+    public void setRotation(double theta) {
+    	this.theta = theta;
+    }    
+    public double getRotation() {
+		return theta;
+	}
+	// EUG -- END
+
+    
+
+	private void _paint0(Graphics2D g,float origAlpha) {
+transformsStack.push(g.getTransform());
+// 
+g.setComposite(AlphaComposite.getInstance(3, 1.0f * origAlpha));
+transformsStack.push(g.getTransform());
+g.transform(new AffineTransform(1.0f, 0.0f, 0.0f, 1.0f, -0.0f, -0.0f));
+// _0
+g.setComposite(AlphaComposite.getInstance(3, 1.0f * origAlpha));
+transformsStack.push(g.getTransform());
+g.transform(new AffineTransform(1.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f));
+// _0_0
+paint = (colorFilter != null) ? colorFilter.filter(new Color(0, 0, 0, 255)) : new Color(0, 0, 0, 255);
+stroke = new BasicStroke(2.0f,1,1,4.0f,null,0.0f);
+shape = new Ellipse2D.Double(2.0, 2.0, 20.0, 20.0);
+g.setPaint(paint);
+g.setStroke(stroke);
+g.draw(shape);
+g.setTransform(transformsStack.pop());
+g.setComposite(AlphaComposite.getInstance(3, 1.0f * origAlpha));
+transformsStack.push(g.getTransform());
+g.transform(new AffineTransform(1.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f));
+// _0_1
+paint = (colorFilter != null) ? colorFilter.filter(new Color(0, 0, 0, 255)) : new Color(0, 0, 0, 255);
+stroke = new BasicStroke(2.0f,1,1,4.0f,null,0.0f);
+if (generalPath == null) {
+   generalPath = new GeneralPath();
+} else {
+   generalPath.reset();
+}
+generalPath.moveTo(9.09f, 9.0f);
+generalPath.curveTo(9.575438f, 7.6200447f, 10.985384f, 6.7914066f, 12.427176f, 7.0387125f);
+generalPath.curveTo(13.868969f, 7.286018f, 14.922179f, 8.537153f, 14.92f, 10.0f);
+generalPath.curveTo(14.92f, 12.0f, 11.92f, 13.0f, 11.92f, 13.0f);
+shape = generalPath;
+g.setPaint(paint);
+g.setStroke(stroke);
+g.draw(shape);
+g.setTransform(transformsStack.pop());
+g.setComposite(AlphaComposite.getInstance(3, 1.0f * origAlpha));
+transformsStack.push(g.getTransform());
+g.transform(new AffineTransform(1.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f));
+// _0_2
+paint = (colorFilter != null) ? colorFilter.filter(new Color(0, 0, 0, 255)) : new Color(0, 0, 0, 255);
+stroke = new BasicStroke(2.0f,1,1,4.0f,null,0.0f);
+shape = new Line2D.Float(12.000000f,17.000000f,12.000000f,17.000000f);
+g.setPaint(paint);
+g.setStroke(stroke);
+g.draw(shape);
+g.setTransform(transformsStack.pop());
+g.setTransform(transformsStack.pop());
+g.setTransform(transformsStack.pop());
+
+}
+
+
+
+	private void innerPaint(Graphics2D g) {
+        float origAlpha = 1.0f;
+        Composite origComposite = g.getComposite();
+        if (origComposite instanceof AlphaComposite) {
+            AlphaComposite origAlphaComposite = 
+                (AlphaComposite)origComposite;
+            if (origAlphaComposite.getRule() == AlphaComposite.SRC_OVER) {
+                origAlpha = origAlphaComposite.getAlpha();
+            }
+        }
+        
+	    _paint0(g, origAlpha);
+
+
+	    shape = null;
+	    generalPath = null;
+	    paint = null;
+	    stroke = null;
+        transformsStack.clear();
+	}
+
+    /**
+     * Returns the X of the bounding box of the original SVG image.
+     * 
+     * @return The X of the bounding box of the original SVG image.
+     */
+    public static double getOrigX() {
+        return 1.0;
+    }
+
+    /**
+     * Returns the Y of the bounding box of the original SVG image.
+     * 
+     * @return The Y of the bounding box of the original SVG image.
+     */
+    public static double getOrigY() {
+        return 1.0;
+    }
+
+	/**
+	 * Returns the width of the bounding box of the original SVG image.
+	 * 
+	 * @return The width of the bounding box of the original SVG image.
+	 */
+	public static double getOrigWidth() {
+		return 22.0;
+	}
+
+	/**
+	 * Returns the height of the bounding box of the original SVG image.
+	 * 
+	 * @return The height of the bounding box of the original SVG image.
+	 */
+	public static double getOrigHeight() {
+		return 22.0;
+	}
+
+	/** The current width of this icon. */
+	private int width;
+
+    /** The current height of this icon. */
+	private int height;
+
+	/**
+	 * Creates a new transcoded SVG image. This is marked as private to indicate that app
+	 * code should be using the {@link #of(int, int)} method to obtain a pre-configured instance.
+	 */
+	private FeatheRhelp_circle() {
+        this.width = (int) getOrigWidth();
+        this.height = (int) getOrigHeight();
+	}
+
+    @Override
+	public int getIconHeight() {
+		return height;
+	}
+
+    @Override
+	public int getIconWidth() {
+		return width;
+	}
+
+	@Override
+	public synchronized void setDimension(Dimension newDimension) {
+		this.width = newDimension.width;
+		this.height = newDimension.height;
+	}
+
+    @Override
+    public boolean supportsColorFilter() {
+        return true;
+    }
+
+    @Override
+    public void setColorFilter(ColorFilter colorFilter) {
+        this.colorFilter = colorFilter;
+    }
+
+    @Override
+	public synchronized void paintIcon(Component c, Graphics g, int x, int y) {
+		Graphics2D g2d = (Graphics2D) g.create();
+		g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
+				RenderingHints.VALUE_ANTIALIAS_ON);
+        g2d.setRenderingHint(RenderingHints.KEY_INTERPOLATION,
+                RenderingHints.VALUE_INTERPOLATION_BICUBIC);
+        if(getRotation()!=0) {
+            g2d.rotate(getRotation(), x+width/2, y+height/2);
+        }
+        if(isReflection()) {
+        	g2d.translate(x+width/2, y+height/2);
+        	g2d.scale(this.rsfx, this.rsfy);
+        	g2d.translate(-x-width/2, -y-height/2);
+        }
+		g2d.translate(x, y);
+
+        double coef1 = (double) this.width / getOrigWidth();
+        double coef2 = (double) this.height / getOrigHeight();
+        double coef = Math.min(coef1, coef2);
+        g2d.clipRect(0, 0, this.width, this.height);
+        g2d.scale(coef, coef);
+        g2d.translate(-getOrigX(), -getOrigY());
+        if (coef1 != coef2) {
+            if (coef1 < coef2) {
+               int extraDy = (int) ((getOrigWidth() - getOrigHeight()) / 2.0);
+               g2d.translate(0, extraDy);
+            } else {
+               int extraDx = (int) ((getOrigHeight() - getOrigWidth()) / 2.0);
+               g2d.translate(extraDx, 0);
+            }
+        }
+        Graphics2D g2ForInner = (Graphics2D) g2d.create();
+        innerPaint(g2ForInner);
+        g2ForInner.dispose();
+        g2d.dispose();
+	}
+    
+    /**
+     * Returns a new instance of this icon with specified dimensions.
+     *
+     * @param width Required width of the icon
+     * @param height Required height of the icon
+     * @return A new instance of this icon with specified dimensions.
+     */
+    public static RadianceIcon of(int width, int height) {
+       FeatheRhelp_circle base = new FeatheRhelp_circle();
+       base.width = width;
+       base.height = height;
+       return base;
+    }
+
+    /**
+     * Returns a new {@link UIResource} instance of this icon with specified dimensions.
+     *
+     * @param width Required width of the icon
+     * @param height Required height of the icon
+     * @return A new {@link UIResource} instance of this icon with specified dimensions.
+     */
+    public static RadianceIconUIResource uiResourceOf(int width, int height) {
+       FeatheRhelp_circle base = new FeatheRhelp_circle();
+       base.width = width;
+       base.height = height;
+       return new RadianceIconUIResource(base);
+    }
+
+    /**
+     * Returns a factory that returns instances of this icon on demand.
+     *
+     * @return Factory that returns instances of this icon on demand.
+     */
+    public static Factory factory() {
+        return FeatheRhelp_circle::new;
+    }
+}
+
--- src/main/java/swingset/OptionPaneDemo.java
+++ src/main/java/swingset/OptionPaneDemo.java
@@ -32,7 +32,15 @@ import org.jdesktop.swingx.JXPanel;
 import org.jdesktop.swingx.icon.RadianceIcon;
 
 /**
- * JOptionPaneDemo
+ * The OptionPane Demo shows examples of using JOptionPane to generate different common option dialog boxes
+ * <p>
+ * It shows usage of
+ * <ul>
+ * <li>{@code JOptionPane.showInputDialog} </li>
+ * <li>{@code JOptionPane.showMessageDialog} </li>
+ * <li>{@code JOptionPane.showConfirmDialog}</li>
+ * <li>{@code JOptionPane.showOptionDialog}</li>
+ * </ul>
  *
  * @author Jeff Dinkins
  * @author EUG https://github.com/homebeaver (reorg)
@@ -49,30 +57,14 @@ class JOptionPane extends JComponent
  3:QUESTION_MESSAGE : "OptionPane.questionIcon"
  -1:PLAIN_MESSAGE : No icon is used.
 
+- there are three nls props
+  UIManager.getString("OptionPane.titleText")          // nls : "Option auswählen" / Select an Option
+  UIManager.getString("OptionPane.messageDialogTitle") // nls : "Meldung" / Message
+  UIManager.getString("OptionPane.inputDialogTitle")   // nls : "Eingabe" / Input
 
  */
 public class OptionPaneDemo extends AbstractDemo {
 
-	private static final boolean USEOPTIONPANEICONS = false;
-	private static final String OPTIONPANEICON[] = new String[] 
-		{ "OptionPane.errorIcon"       // for JOptionPane.ERROR_MESSAGE , red octagon with exclamation mark
-		, "OptionPane.informationIcon" // ... , blue circle with i
-		, "OptionPane.warningIcon"     // ... , yellow triangle with exclamation mark
-		, "OptionPane.questionIcon"    // ... , green square with question mark
-		};
-	private static final String ALTFEATHERICON[] = new String[] 
-		{ "org.jdesktop.swingx.demos.svg.FeatheRalert_octagon"
-		, "org.jdesktop.swingx.demos.svg.FeatheRhelp_circle"
-		, "org.jdesktop.swingx.demos.svg.FeatheRalert_triangle"
-		, "org.jdesktop.swingx.demos.svg.FeatheRinfo"
-		};
-	private static final Color COLOROFICON[] = new Color[] 
-		{ Color.RED
-		, Color.BLUE
-		, Color.ORANGE
-		, Color.GREEN
-		};
-	
 	/**
 	 * this is used in DemoAction to build the demo toolbar
 	 */
@@ -80,7 +72,35 @@ public class OptionPaneDemo extends AbstractDemo {
 
 	private static final long serialVersionUID = 3164409244554337243L;
 	private static final boolean CONTROLLER_IN_PRESENTATION_FRAME = false;
+	private static final boolean USE_OPTIONPANE_ICONS = false;
 
+	private static final String OPTIONPANE_TITLETEXT = "OptionPane.titleText";
+	private static final String OPTIONPANE_MESSAGE   = "OptionPane.messageDialogTitle";
+	private static final String OPTIONPANE_INPUT     = "OptionPane.inputDialogTitle";
+	private String getUIString(Object key) {
+		return UIManager.getString(key, getLocale());
+	}
+	
+	private static final String OPTIONPANE_ICON[] = new String[] 
+		{ "OptionPane.errorIcon"       // for JOptionPane.ERROR_MESSAGE , red octagon with x
+		, "OptionPane.informationIcon" // ... , blue circle with i
+		, "OptionPane.warningIcon"     // ... , yellow triangle with exclamation mark
+		, "OptionPane.questionIcon"    // ... , green square with question mark
+		};
+	// alternative monochrome icons with colors:
+	private static final String FEATHER_ICON_CLASS[] = new String[] 
+		{ "org.jdesktop.swingx.demos.svg.FeatheRalert_octagon"
+		, "org.jdesktop.swingx.demos.svg.FeatheRinfo"
+		, "org.jdesktop.swingx.demos.svg.FeatheRalert_triangle"
+		, "org.jdesktop.swingx.demos.svg.FeatheRhelp_circle"
+		};
+	private static final Color COLOR_OF_ICON[] = new Color[] 
+		{ Color.RED
+		, Color.BLUE
+		, Color.ORANGE
+		, Color.GREEN
+		};
+	
     /**
      * main method allows us to run as a standalone demo.
      */
@@ -145,6 +165,7 @@ public class OptionPaneDemo extends AbstractDemo {
 
         buttonPanel.add(Box.createRigidArea(VGAP30));
 
+        buttonPanel.add(createErrorMessageButton());     buttonPanel.add(Box.createRigidArea(VGAP15));
         buttonPanel.add(createInputDialogButton());      buttonPanel.add(Box.createRigidArea(VGAP15));
         buttonPanel.add(createWarningDialogButton());    buttonPanel.add(Box.createRigidArea(VGAP15));
         buttonPanel.add(createMessageDialogButton());    buttonPanel.add(Box.createRigidArea(VGAP15));
@@ -158,51 +179,70 @@ public class OptionPaneDemo extends AbstractDemo {
         return boxPane;
     }
 
-    private Icon getIcon(int messageType) {
-    	if(USEOPTIONPANEICONS) return UIManager.getIcon(OPTIONPANEICON[messageType]);
-        // alternative icon in ControlPane, 
-    	// in presentation pane JOptionPane.showMessageDialog is used... TODO
-    	Class<?> iconClass = null;
-		try {
-			iconClass = Class.forName(ALTFEATHERICON[messageType]);  // throws ClassNotFoundException
-		} catch (ClassNotFoundException e) {
-			e.printStackTrace();
-			return UIManager.getIcon(OPTIONPANEICON[messageType]);
-		}
-    	RadianceIcon icon = null;
-    	try {
-			Method method = iconClass.getMethod("of", int.class, int.class);
-			Object o = method.invoke(null, RadianceIcon.BUTTON_ICON, RadianceIcon.BUTTON_ICON);
-			icon = (RadianceIcon)o; // ClassCastException
-		} catch (NoSuchMethodException | SecurityException 
-				| IllegalAccessException | IllegalArgumentException | InvocationTargetException e) {
-			e.printStackTrace();
-		} catch (ClassCastException e) {
-			e.printStackTrace();
-			return UIManager.getIcon(OPTIONPANEICON[messageType]);
-		}
-    	icon.setColorFilter(color -> COLOROFICON[messageType]);
-	    return icon;
+    private JButton createErrorMessageButton() {
+        @SuppressWarnings("serial")
+		Action a = new AbstractAction(getBundleString("errorbutton")) {
+        	@Override	
+            public void actionPerformed(ActionEvent e) {
+                JOptionPane.showMessageDialog
+            	( OptionPaneDemo.this                  // parentComponent
+            	, getBundleString("errormessage")      // Object message 
+            	, getUIString(OPTIONPANE_MESSAGE)      // String title, nls : "Meldung" / Message
+            	, JOptionPane.ERROR_MESSAGE
+            	, getIcon(JOptionPane.ERROR_MESSAGE)
+                );
+            }
+        };
+        a.putValue(Action.LARGE_ICON_KEY, getIcon(JOptionPane.ERROR_MESSAGE));
+        return createButton(a);
     }
-    
+
+    private JButton createInputDialogButton() {
+        @SuppressWarnings("serial")
+        Action a = new AbstractAction(getBundleString("inputbutton")) {
+            public void actionPerformed(ActionEvent e) {
+            	/* BUG TODO
+während getUIString(key) locale berüchsichtigt (auch nach Änderung in demo bar), wenn aus jar gestartet
+liefert getBundleString immer dieselbe Sprache!!!
+            	 */
+                String result = (String)JOptionPane.showInputDialog
+                	( OptionPaneDemo.this
+                	, getBundleString("inputquestion")      // Object message                	
+                	, getUIString(OPTIONPANE_INPUT)         // String title, nls : "Eingabe" / Input
+                	, JOptionPane.QUESTION_MESSAGE
+                	, getIcon(JOptionPane.QUESTION_MESSAGE) // the Icon to display
+                	, null, null                            // selectionValues, initialSelectionValue
+                	);
+                if ((result != null) && (result.length() > 0)) {
+                    JOptionPane.showMessageDialog
+                    	( OptionPaneDemo.this                               // parentComponent
+                    	, result + ": " + getBundleString("inputresponse")  // Object message 
+                    	, getUIString(OPTIONPANE_MESSAGE)                   // String title, nls : "Meldung" / Message
+                    	, JOptionPane.INFORMATION_MESSAGE
+                    	, getIcon(JOptionPane.INFORMATION_MESSAGE)
+                        );
+                }
+            }
+        };
+        a.putValue(Action.LARGE_ICON_KEY, getIcon(JOptionPane.QUESTION_MESSAGE));
+        return createButton(a);
+    }
+
     private JButton createWarningDialogButton() {
         @SuppressWarnings("serial")
 		Action a = new AbstractAction(getBundleString("warningbutton")) {
         	@Override	
             public void actionPerformed(ActionEvent e) {
-                JOptionPane.showMessageDialog(
-                	OptionPaneDemo.this,
-                    getBundleString("warningtext"),
-                    getBundleString("warningtitle"),
-                    JOptionPane.WARNING_MESSAGE
-//                    ,
-//                    getIcon(JOptionPane.WARNING_MESSAGE)
+                JOptionPane.showMessageDialog
+                	( OptionPaneDemo.this
+                	, getBundleString("warningtext")
+                	, getBundleString("warningtitle")
+                	, JOptionPane.WARNING_MESSAGE
+                	, getIcon(JOptionPane.WARNING_MESSAGE
+                	)
                 );
             }
         };
-        // see protected Icon BasicOptionPaneUI.getIconForType(int messageType)
-        // Icon errorIcon = UIManager.getIcon(OPTIONPANEICON[JOptionPane.ERROR_MESSAGE]);
-        // RadianceIcon ricon = FeatheRalert_octagon.of(RadianceIcon.BUTTON_ICON, RadianceIcon.BUTTON_ICON);
         a.putValue(Action.LARGE_ICON_KEY, getIcon(JOptionPane.WARNING_MESSAGE));
         return createButton(a);
     }
@@ -214,43 +254,17 @@ public class OptionPaneDemo extends AbstractDemo {
             String imagesrc = "<img src=\"" + img + "\" width=\"284\" height=\"100\">";
             String message = getBundleString("messagetext");
             public void actionPerformed(ActionEvent e) {
-                JOptionPane.showMessageDialog( OptionPaneDemo.this,
-                    "<html>" + imagesrc + "<br><center>" + message + "</center><br></html>"
+                JOptionPane.showMessageDialog
+                	( OptionPaneDemo.this
+                    , "<html>" + imagesrc + "<br><center>" + message + "</center><br></html>"
+//  not visible:    UIManager.getString("OptionPane.messageDialogTitle", OptionPaneDemo.this),
+                    , getUIString(OPTIONPANE_MESSAGE)
+                    , JOptionPane.INFORMATION_MESSAGE
+                    , getIcon(JOptionPane.INFORMATION_MESSAGE)
                 );
             }
         };
         a.putValue(Action.LARGE_ICON_KEY, getIcon(JOptionPane.INFORMATION_MESSAGE));
-        return createButton(a);
-    }
-
-    private JButton createConfirmDialogButton() {
-        @SuppressWarnings("serial")
-        Action a = new AbstractAction(getBundleString("confirmbutton")) {
-            public void actionPerformed(ActionEvent e) {
-                int result = JOptionPane.showConfirmDialog(OptionPaneDemo.this, getBundleString("confirmquestion"));
-                if(result == JOptionPane.YES_OPTION) {
-                    JOptionPane.showMessageDialog(OptionPaneDemo.this, getBundleString("confirmyes"));
-                } else if(result == JOptionPane.NO_OPTION) {
-                    JOptionPane.showMessageDialog(OptionPaneDemo.this, getBundleString("confirmno"));
-                }
-            }
-        };
-        a.putValue(Action.LARGE_ICON_KEY, getIcon(JOptionPane.QUESTION_MESSAGE));
-        return createButton(a);
-    }
-
-    private JButton createInputDialogButton() {
-        @SuppressWarnings("serial")
-        Action a = new AbstractAction(getBundleString("inputbutton")) {
-            public void actionPerformed(ActionEvent e) {
-                String result = JOptionPane.showInputDialog(OptionPaneDemo.this, getBundleString("inputquestion"));
-                if ((result != null) && (result.length() > 0)) {
-                    JOptionPane.showMessageDialog(OptionPaneDemo.this,
-                                    result + ": " + getBundleString("inputresponse"));
-                }
-            }
-        };
-        a.putValue(Action.LARGE_ICON_KEY, getIcon(JOptionPane.QUESTION_MESSAGE));
         return createButton(a);
     }
 
@@ -274,7 +288,7 @@ public class OptionPaneDemo extends AbstractDemo {
 
                 message[3] = getBundleString("componentmessage2");
 
-                // Options
+                // Options from nls props:
                 String[] options = {
                     getBundleString("component_op1"),
                     getBundleString("component_op2"),
@@ -288,22 +302,46 @@ public class OptionPaneDemo extends AbstractDemo {
                     getBundleString("componenttitle"),          // the title of the dialog window
                     JOptionPane.DEFAULT_OPTION,                 // option type
                     JOptionPane.INFORMATION_MESSAGE,            // message type
-                    null,                                       // optional icon, use null to use the default icon
+                    getIcon(JOptionPane.INFORMATION_MESSAGE),   // optional icon, use null to use the default icon
                     options,                                    // options string array, will be made into buttons
                     options[3]                                  // option that should be made into a default button
                 );
                 switch(result) {
                    case 0: // yes
-                     JOptionPane.showMessageDialog(OptionPaneDemo.this, getBundleString("component_r1"));
+                     JOptionPane.showMessageDialog
+	                 	( OptionPaneDemo.this                  // parentComponent
+	                 	, getBundleString("component_r1")      // Object message 
+	                 	, getUIString(OPTIONPANE_MESSAGE)      // String title, nls : "Meldung" / Message
+	                 	, JOptionPane.INFORMATION_MESSAGE
+	                 	, getIcon(JOptionPane.INFORMATION_MESSAGE)
+	                    );
                      break;
                    case 1: // no
-                     JOptionPane.showMessageDialog(OptionPaneDemo.this, getBundleString("component_r2"));
+                     JOptionPane.showMessageDialog
+	                 	( OptionPaneDemo.this
+	                 	, getBundleString("component_r2")
+	                 	, getUIString(OPTIONPANE_MESSAGE)
+	                 	, JOptionPane.INFORMATION_MESSAGE
+	                 	, getIcon(JOptionPane.INFORMATION_MESSAGE)
+	                    );
                      break;
                    case 2: // maybe
-                     JOptionPane.showMessageDialog(OptionPaneDemo.this, getBundleString("component_r3"));
+                       JOptionPane.showMessageDialog
+  	                 	( OptionPaneDemo.this
+  	                 	, getBundleString("component_r3")
+  	                 	, getUIString(OPTIONPANE_MESSAGE)
+  	                 	, JOptionPane.INFORMATION_MESSAGE
+  	                 	, getIcon(JOptionPane.INFORMATION_MESSAGE)
+  	                    );
                      break;
                    case 3: // probably
-                     JOptionPane.showMessageDialog(OptionPaneDemo.this, getBundleString("component_r4"));
+                       JOptionPane.showMessageDialog
+  	                 	( OptionPaneDemo.this
+  	                 	, getBundleString("component_r4")
+  	                 	, getUIString(OPTIONPANE_MESSAGE)
+  	                 	, JOptionPane.INFORMATION_MESSAGE
+  	                 	, getIcon(JOptionPane.INFORMATION_MESSAGE)
+  	                    );
                      break;
                    default:
                      break;
@@ -315,6 +353,68 @@ public class OptionPaneDemo extends AbstractDemo {
         return createButton(a);
     }
 
+    private JButton createConfirmDialogButton() {
+        @SuppressWarnings("serial")
+        Action a = new AbstractAction(getBundleString("confirmbutton")) {
+            public void actionPerformed(ActionEvent e) {
+                int result = JOptionPane.showConfirmDialog
+                		( OptionPaneDemo.this
+                		, getBundleString("confirmquestion")
+                		, getUIString(OPTIONPANE_TITLETEXT)
+                		, JOptionPane.YES_NO_CANCEL_OPTION
+                		, JOptionPane.QUESTION_MESSAGE
+                		, getIcon(JOptionPane.QUESTION_MESSAGE)
+                		);
+                if(result == JOptionPane.YES_OPTION) {
+                    JOptionPane.showMessageDialog
+	                 	( OptionPaneDemo.this
+	                 	, getBundleString("confirmyes")
+	                 	, getUIString(OPTIONPANE_MESSAGE)
+	                 	, JOptionPane.INFORMATION_MESSAGE
+	                 	, getIcon(JOptionPane.INFORMATION_MESSAGE)
+	                    );
+                } else if(result == JOptionPane.NO_OPTION) {
+                    JOptionPane.showMessageDialog
+	                 	( OptionPaneDemo.this
+	                 	, getBundleString("confirmno")
+	                 	, getUIString(OPTIONPANE_MESSAGE)
+	                 	, JOptionPane.INFORMATION_MESSAGE
+	                 	, getIcon(JOptionPane.INFORMATION_MESSAGE)
+	                    );
+                }
+            }
+        };
+        a.putValue(Action.LARGE_ICON_KEY, getIcon(JOptionPane.QUESTION_MESSAGE));
+        return createButton(a);
+    }
+
+    private Icon getIcon(int messageType) {
+    	if(USE_OPTIONPANE_ICONS) return UIManager.getIcon(OPTIONPANE_ICON[messageType]);
+        // alternative icon in ControlPane, 
+    	// in presentation pane JOptionPane.showInputDialog, .showMessageDialog, ...  is used
+    	Class<?> iconClass = null;
+		try {
+			iconClass = Class.forName(FEATHER_ICON_CLASS[messageType]);  // throws ClassNotFoundException
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+			return UIManager.getIcon(OPTIONPANE_ICON[messageType]);
+		}
+    	RadianceIcon icon = null;
+    	try {
+			Method method = iconClass.getMethod("of", int.class, int.class);
+			Object o = method.invoke(null, RadianceIcon.BUTTON_ICON, RadianceIcon.BUTTON_ICON);
+			icon = (RadianceIcon)o; // ClassCastException
+		} catch (NoSuchMethodException | SecurityException 
+				| IllegalAccessException | IllegalArgumentException | InvocationTargetException e) {
+			e.printStackTrace();
+		} catch (ClassCastException e) {
+			e.printStackTrace();
+			return UIManager.getIcon(OPTIONPANE_ICON[messageType]);
+		}
+    	icon.setColorFilter(color -> COLOR_OF_ICON[messageType]);
+	    return icon;
+    }
+    
     /**
      * createButton for Action
      * @param a Action
