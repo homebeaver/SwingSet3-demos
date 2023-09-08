2d748a52a4f2568cf742aeb21dd99acbede63149
homebeaver <homebeaver@outlook.de>
2023-09-08 10:34:09 +0200

    package-info

---
+++ src/main/java/org/jdesktop/swingx/demos/package-info.java
@@ -0,0 +1,7 @@
+/*
+ * this package contains all swingset3 demos
+ * - the origin/inception was a subproject /swingx-demos/swingx-demos-content/ 
+ *   see https://github.com/homebeaver/SwingSet/wiki/Maven-projects-and-dependences#demos
+ * - the demos are grouped into categories: functionality, data, containers , ...
+ */
+package org.jdesktop.swingx.demos;
