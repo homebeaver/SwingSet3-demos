4abaa61344a7fd17531c3117235aa8ad51b2dd56
homebeaver <homebeaver@outlook.de>
2023-09-27 20:21:18 +0200

    reorg CustomComboBoxDemo
    https://github.com/homebeaver/SwingSet/issues/32

--- src/main/java/org/jdesktop/swingx/demos/combobox/CustomComboBoxDemo.java
+++ src/main/java/org/jdesktop/swingx/demos/combobox/CustomComboBoxDemo.java
@@ -30,75 +30,55 @@
  */ 
 package org.jdesktop.swingx.demos.combobox;
 
-import java.awt.*;
-import java.awt.event.*;
-import javax.swing.*;
+import java.awt.BorderLayout;
+import java.awt.Component;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.net.URL;
+
+import javax.swing.BorderFactory;
+import javax.swing.ImageIcon;
+import javax.swing.JComboBox;
+import javax.swing.JComponent;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JList;
+import javax.swing.JPanel;
+import javax.swing.ListCellRenderer;
+import javax.swing.SwingUtilities;
+import javax.swing.UIManager;
+
+import swingset.plaf.LaFUtils;
 
 /*
- * CustomComboBoxDemo.java uses the following files:
+ * copied from https://docs.oracle.com/javase/tutorial/uiswing/examples/components/index.html#CustomComboBoxDemo
+ * CustomComboBoxDemo.java uses the files like ComboBoxDemo.java:
  *   images/Bird.gif
  *   images/Cat.gif
  *   images/Dog.gif
  *   images/Rabbit.gif
  *   images/Pig.gif
  */
+@SuppressWarnings("serial")
 public class CustomComboBoxDemo extends JPanel {
-    ImageIcon[] images;
-    String[] petStrings = {"Bird", "Cat", "Dog", "Rabbit", "Pig"};
-
-    /*
-     * Despite its use of EmptyBorder, this panel makes a fine content
-     * pane because the empty border just increases the panel's size
-     * and is "painted" on top of the panel's normal background.  In
-     * other words, the JPanel fills its entire background if it's
-     * opaque (which it is by default); adding a border doesn't change
-     * that.
-     */
-    public CustomComboBoxDemo() {
-        super(new BorderLayout());
-
-        //Load the pet images and create an array of indexes.
-        images = new ImageIcon[petStrings.length];
-        Integer[] intArray = new Integer[petStrings.length];
-        for (int i = 0; i < petStrings.length; i++) {
-            intArray[i] = new Integer(i);
-            images[i] = createImageIcon("images/" + petStrings[i] + ".gif");
-            if (images[i] != null) {
-                images[i].setDescription(petStrings[i]);
-            }
-        }
-
-        //Create the combo box.
-        JComboBox petList = new JComboBox(intArray);
-        ComboBoxRenderer renderer= new ComboBoxRenderer();
-        renderer.setPreferredSize(new Dimension(200, 130));
-        petList.setRenderer(renderer);
-        petList.setMaximumRowCount(3);
-
-        //Lay out the demo.
-        add(petList, BorderLayout.PAGE_START);
-        setBorder(BorderFactory.createEmptyBorder(20,20,20,20));
-    }
-
-    /** Returns an ImageIcon, or null if the path was invalid. */
-    protected static ImageIcon createImageIcon(String path) {
-        java.net.URL imgURL = CustomComboBoxDemo.class.getResource(path);
-        if (imgURL != null) {
-            return new ImageIcon(imgURL);
-        } else {
-            System.err.println("Couldn't find file: " + path);
-                return null;
-        }
+	
+    public static void main(String[] args) {
+        //Schedule a job for the event-dispatching thread:
+        //creating and showing this application's GUI.
+    	if(args.length>0) LaFUtils.setLAF(args[0]);
+        SwingUtilities.invokeLater( () -> {
+            createAndShowGUI();
+        });
     }
 
     /**
-     * Create the GUI and show it.  For thread safety,
-     * this method should be invoked from the
-     * event-dispatching thread.
+     * Create the GUI and show it.
+     * For thread safety, this method should be invoked from the event-dispatching thread.
      */
     private static void createAndShowGUI() {
+    	UIManager.put("swing.boldMetal", Boolean.FALSE); // turn off bold fonts in Metal
         //Create and set up the window.
-        JFrame frame = new JFrame("CustomComboBoxDemo");
+        JFrame frame = new JFrame("Provides a custom renderer for a combo box.");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         //Create and set up the content pane.
@@ -111,18 +91,57 @@ public class CustomComboBoxDemo extends JPanel {
         frame.setVisible(true);
     }
 
-    public static void main(String[] args) {
-        //Schedule a job for the event-dispatching thread:
-        //creating and showing this application's GUI.
-        javax.swing.SwingUtilities.invokeLater(new Runnable() {
-            public void run() {
-                createAndShowGUI();
+    ImageIcon[] images;
+    // add Oops without image
+    String[] petStrings = {"Bird", "Cat", "Dog", "Rabbit", "Pig", "Oops"};
+
+    /*
+     * Despite its use of EmptyBorder, this panel makes a fine content pane 
+     * because the empty border just increases the panel's size
+     * and is "painted" on top of the panel's normal background.  
+     * In other words, the JPanel fills its entire background if it's opaque (which it is by default); 
+     * adding a border doesn't change that.
+     */
+    public CustomComboBoxDemo() {
+        super(new BorderLayout());
+
+        //Load the pet images and create an array of indexes.
+        images = new ImageIcon[petStrings.length];
+        Integer[] intArray = new Integer[petStrings.length];
+        for (int i = 0; i < petStrings.length; i++) {
+            intArray[i] = Integer.valueOf(i);
+            images[i] = createImageIcon("resources/images/" + petStrings[i] + ".gif");
+            if (images[i] != null) {
+                images[i].setDescription(petStrings[i]);
             }
-        });
+        }
+
+        // Create the combo box, modeled with Integers! The renderer maps the ints to petStrings and images
+        JComboBox<Integer> petList = new JComboBox<>(intArray);
+        //Indices start at 0, so 4 specifies the pig.
+        petList.setSelectedIndex(4);
+        ComboBoxRenderer renderer= new ComboBoxRenderer();
+        renderer.setPreferredSize(new Dimension(200, 130));
+        petList.setRenderer(renderer);
+        petList.setMaximumRowCount(3);
+
+        //Lay out the demo.
+        add(petList, BorderLayout.PAGE_START);
+        setBorder(BorderFactory.createEmptyBorder(20,20,20,20));
     }
 
-    class ComboBoxRenderer extends JLabel
-                           implements ListCellRenderer {
+    /** Returns an ImageIcon, or null if the path was invalid. */
+    private ImageIcon createImageIcon(String path) {
+    	URL imgURL = ComboBoxDemo.class.getResource(path);
+        if (imgURL != null) {
+            return new ImageIcon(imgURL);
+        } else {
+            System.err.println("Couldn't find file: " + path);
+            return null;
+        }
+    }
+
+    class ComboBoxRenderer extends JLabel implements ListCellRenderer<Integer> {
         private Font uhOhFont;
 
         public ComboBoxRenderer() {
@@ -132,19 +151,15 @@ public class CustomComboBoxDemo extends JPanel {
         }
 
         /*
-         * This method finds the image and text corresponding
-         * to the selected value and returns the label, set up
-         * to display the text and image.
+         * This method finds the image and text corresponding to the selected value 
+         * and returns the label, set up to display the text and image.
          */
-        public Component getListCellRendererComponent(
-                                           JList list,
-                                           Object value,
-                                           int index,
-                                           boolean isSelected,
-                                           boolean cellHasFocus) {
-            //Get the selected index. (The index param isn't
-            //always valid, so just use the value.)
-            int selectedIndex = ((Integer)value).intValue();
+		@Override
+		public Component getListCellRendererComponent(JList<? extends Integer> list, Integer value, 
+				int index, boolean isSelected, boolean cellHasFocus) {
+			
+            // Get the selected index. (The index param isn't always valid, so just use the value.)
+            int selectedIndex = value.intValue();
 
             if (isSelected) {
                 setBackground(list.getSelectionBackground());
@@ -154,7 +169,7 @@ public class CustomComboBoxDemo extends JPanel {
                 setForeground(list.getForeground());
             }
 
-            //Set the icon and text.  If icon was null, say so.
+            // Set the icon and text. If icon was null, say so.
             ImageIcon icon = images[selectedIndex];
             String pet = petStrings[selectedIndex];
             setIcon(icon);
@@ -162,14 +177,13 @@ public class CustomComboBoxDemo extends JPanel {
                 setText(pet);
                 setFont(list.getFont());
             } else {
-                setUhOhText(pet + " (no image available)",
-                            list.getFont());
+                setUhOhText(pet + " (no image available)", list.getFont());
             }
 
             return this;
-        }
+		}
 
-        //Set the font and text when no image was found.
+        // Set the font and text when no image was found.
         protected void setUhOhText(String uhOhText, Font normalFont) {
             if (uhOhFont == null) { //lazily create this font
                 uhOhFont = normalFont.deriveFont(Font.ITALIC);
@@ -177,5 +191,6 @@ public class CustomComboBoxDemo extends JPanel {
             setFont(uhOhFont);
             setText(uhOhText);
         }
+
     }
 }
