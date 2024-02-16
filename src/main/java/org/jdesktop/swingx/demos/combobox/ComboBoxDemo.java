40a304b5452f88f024ab9effcdcb3c5e2fb2add1
homebeaver <homebeaver@outlook.de>
2024-02-16 18:21:06 +0100

    ComboBoxDemo which uses an uneditable combo box with pets
    long Text as item 4 : longest DisplayValue to calculate cell size
    TODO rename to XComboBoxPets

--- src/main/java/org/jdesktop/swingx/demos/combobox/ComboBoxDemo.java
+++ src/main/java/org/jdesktop/swingx/demos/combobox/ComboBoxDemo.java
@@ -34,25 +34,35 @@ import java.awt.BorderLayout;
 import java.awt.Component;
 import java.awt.Dimension;
 import java.awt.Font;
+import java.io.Serializable;
 import java.net.URL;
+import java.util.Arrays;
 
 import javax.swing.BorderFactory;
 import javax.swing.Icon;
 import javax.swing.ImageIcon;
-import javax.swing.JComboBox;
 import javax.swing.JComponent;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JList;
 import javax.swing.JPanel;
+import javax.swing.ListCellRenderer;
+import javax.swing.ListModel;
+import javax.swing.RowSorter;
 import javax.swing.SwingUtilities;
 import javax.swing.UIManager;
 import javax.swing.border.Border;
-import javax.swing.plaf.basic.BasicComboBoxRenderer;
+import javax.swing.border.EmptyBorder;
+
+import org.jdesktop.swingx.JXComboBox;
+import org.jdesktop.swingx.icon.ChevronIcon;
+import org.jdesktop.swingx.icon.RadianceIcon;
+import org.jdesktop.swingx.icon.SizingConstants;
+import org.jdesktop.swingx.renderer.ComboBoxContext;
 
 import swingset.plaf.LaFUtils;
 
-/* ComboBoxDemo which uses an uneditable combo box.
+/* ComboBoxDemo which uses an uneditable combo box with pets TODO rename to XComboBoxPets
  * copied from https://docs.oracle.com/javase/tutorial/uiswing/examples/components/index.html#ComboBoxDemo
  * 
  * ComboBoxDemo.java uses these additional files:
@@ -66,9 +76,9 @@ import swingset.plaf.LaFUtils;
 public class ComboBoxDemo extends JPanel {
 	
     public static void main(String[] args) {
+    	if(args.length>0) LaFUtils.setLAFandTheme(Arrays.asList(args));
         //Schedule a job for the event-dispatching thread:
         //creating and showing this application's GUI.
-    	if(args.length>0) LaFUtils.setLAF(args[0]);
         SwingUtilities.invokeLater( () -> {
             createAndShowGUI();
         });
@@ -95,27 +105,71 @@ public class ComboBoxDemo extends JPanel {
     }
 
     JLabel picture;
+    JXComboBox<String> cb;
 
     public ComboBoxDemo() {
         super(new BorderLayout());
 
-        String[] petStrings = { "Bird", "Cat", "Dog", "Rabbit", "Pig" };
+        String[] emptyStrings = { };
+        String[] petStrings = { "Rabbit", "Bird", "Pig", "Cat"
+        		, "Text: Create the combo box, select the item at index 4."
+//        		, "Dog", "Rabbit", "Pig" };
+        };
 
-        //Create the combo box, select the item at index 4.
-        //Indices start at 0, so 4 specifies the pig.
-        JComboBox<String> petList = new JComboBox<>(petStrings);
-        petList.setSelectedIndex(4);
+        //Create a sorted combo box, select the item at index 4.
+        //Indices start at 0, so 4 specifies the long text.
+        JXComboBox<String> petList = new JXComboBox<>(petStrings, true);
+        cb = petList;
+        RowSorter<? extends ListModel<String>> rs = petList.getRowSorter();
+        System.out.println("ComboBoxDemo petList:"+petList
+        	+ "\n RowSorter:"+rs
+        	+ "\n RowSorter ViewRowCount/ModelRowCount="+rs.getViewRowCount()+"/"+rs.getModelRowCount()
+        	+ "\n RowSorter 0 at view ="+rs.convertRowIndexToView(0) + " expected 3"
+        	+ "\n RowSorter 1 at view ="+rs.convertRowIndexToView(1) + " expected 0"
+        	+ "\n RowSorter 2 at view ="+rs.convertRowIndexToView(2) + " expected 2"
+        	+ "\n RowSorter 3 at view ="+rs.convertRowIndexToView(3) + " expected 1"
+        	+ "\n RowSorter 4 at view ="+rs.convertRowIndexToView(4) + " expected 4"
+        	+ "\n petList.PrototypeDisplayValue:"+petList.getPrototypeDisplayValue()
+        );
+        // JVM disables assertion validation by default: use -enableassertions to enable!
+        assert(rs.convertRowIndexToView(0)==3);
+        assert(rs.convertRowIndexToView(1)==0);
+        assert(rs.convertRowIndexToView(2)==2);
+        assert(rs.convertRowIndexToView(3)==1);
+        assert(rs.convertRowIndexToView(4)==4);
+        
+        // is PrototypeDisplayValue kürzer als longest DisplayValue aka petStrings, so wird longest abgeschnitten!!!
+//        petList.setPrototypeDisplayValue("a PrototypeDisplayValue");
+        petList.setToolTipText("Choose an animal name from the combo box to view its picture");
+        // nicht notwendig, da autoCreateRowSorter==true im ctor
+        //petList.setRowSorter(new ListSortController<ListModel<String>>(petList.getModel()), SortOrder.ASCENDING);
+        
+        // determine selected item (-1 indicates no selection, 0 is the default)
+        if(petList.getModel().getSize()>0) petList.setSelectedIndex(1);
+        
         petList.addActionListener(ae -> {
             String petName = (String)petList.getSelectedItem();
             updateLabel(petName);
         });
-        petList.setRenderer(new MyComboBoxRenderer());
+        
+//        petList.setEnabled(false);
+        // setComboBoxIcon: 
+		RadianceIcon icon = ChevronIcon.of(SizingConstants.XS, SizingConstants.XS);
+		icon.setReflection(true); // for ChevronIcon same effect as icon.setRotation(RadianceIcon.SOUTH)
+        petList.setComboBoxIcon(icon, ChevronIcon.of(SizingConstants.XS, SizingConstants.XS));
+//        petList.setComboBoxIcon(FeatheRuser.of(SizingConstants.SMALL_ICON, SizingConstants.SMALL_ICON));
+//        JXIcon personIcon = FeatheRuser.of(SizingConstants.SMALL_ICON, SizingConstants.SMALL_ICON);      
+//        petList.setComboBoxIcon(personIcon);
+        
+//        petList.setEditable(true);
+//        petList.setRenderer(new MyComboBoxRenderer());
 
         //Set up the picture.
         picture = new JLabel();
         picture.setFont(picture.getFont().deriveFont(Font.ITALIC));
         picture.setHorizontalAlignment(JLabel.CENTER);
-        updateLabel(petStrings[petList.getSelectedIndex()]);
+        int selIndex = petList.getSelectedIndex();
+        updateLabel(selIndex>-1 ? petStrings[selIndex] : "not defined");
         picture.setBorder(BorderFactory.createEmptyBorder(10,0,0,0));
 
         //The preferred size is hard-coded to be the width of the
@@ -156,25 +210,67 @@ public class ComboBoxDemo extends JPanel {
      * A subclass of BasicComboBoxRenderer to override the renderer property
      * - border of the Label : EmptyNoFocusBorder,
      *          of the List : EtchedBorder
+public class BasicComboBoxRenderer extends JLabel
+implements ListCellRenderer<Object>, Serializable {
+
+
+public class DefaultComboBoxRenderer<E> extends AbstractRenderer implements ListCellRenderer<E> {
+ComponentProvider<JLabel> componentProvider = new LabelProvider();
+DefaultComboBoxRenderer renderer = new DefaultComboBoxRenderer(ComponentProvider<?> componentProvider);
      */
-    class MyComboBoxRenderer extends BasicComboBoxRenderer {
+//    class MyComboBoxRenderer extends BasicComboBoxRenderer {
+    class MyComboBoxRenderer extends JLabel implements ListCellRenderer<Object>, Serializable {
 
         static Border etchedNoFocusBorder = BorderFactory.createEtchedBorder();
 
+        ComboBoxContext cc;
         public MyComboBoxRenderer() {
             super();
+//            CellContext cc = new ComboBoxContext();
+            cc = new ComboBoxContext();
         }
 
         Border getEmptyNoFocusBorder() {
-        	return BasicComboBoxRenderer.noFocusBorder;
+        	return new EmptyBorder(1, 1, 1, 1);
         }
 
+        /*
+called with index -1 in 
+- BasicXComboBoxUI.getDisplaySize n times (...false, false) to calculate the dimension
+            	renderer.getListCellRendererComponent(listBox, prototypeValue, -1, false, false)
+                    Component c = renderer.getListCellRendererComponent(listBox, value, -1, false, false);
+
+- BasicXComboBoxUI.getDefaultSize 1x (...false, false) to calculate the dimension
+        Component comp = getDefaultListCellRenderer().getListCellRendererComponent(listBox, " ", -1, false, false);
+
+- getBaseline ??? with (...false, false)
+                Component component = renderer.getListCellRendererComponent(listBox, value, -1, false, false);
+
+- getBaselineResizeBehavior
+                Component component = renderer.getListCellRendererComponent(listBox, value, -1, false, false);
+
+- BasicXComboBoxUI.paintCurrentValue (...isSelected, cellHasFocus)
+            c = renderer.getListCellRendererComponent( listBox,
+                                                       comboBox.getSelectedItem(),
+                                                     -1, ...
+
+im renderer: wird bei index == -1 
+         */
         @Override
 		public Component getListCellRendererComponent(JList<?> list, Object value, 
 				int index, boolean isSelected, boolean cellHasFocus) {
-        	
-        	// index -1 when called in BasicComboBoxUI.getDisplaySize()
-        	//      >=0 when called in BasicListUI.updateLayoutState()
+
+//            public void installContext(JComboBox<?> component, Object value, int row, int column,
+//                    boolean selected, boolean focused, boolean expanded, boolean leaf) {
+            cc.installContext(ComboBoxDemo.this.cb, value, index, -1, isSelected, cellHasFocus, false, false);
+           
+
+    		System.out.println("ComboBoxDemo getListCellRendererComponent: list:"+list
+    			+"\n value:"+value+"/"+(value==null?"null":value.getClass())
+    			+"\n index="+index+" , isSelected="+isSelected+" , cellHasFocus="+cellHasFocus
+    			+"\n ListCellRendererComponent hashCode=@"+Integer.toHexString(this.hashCode())
+    				);
+    		
 			setBorder(index == -1 ? getEmptyNoFocusBorder() : etchedNoFocusBorder);
 
 			if (isSelected) {
